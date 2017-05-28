a72a301be5a77ecae323577d43a634e8713842ed
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-29 01:49:37 +0400

    Add e107_languages/Malayalam/lan_user_extended.php

---
+++ e107_languages/Malayalam/lan_user_extended.php
@@ -0,0 +1,58 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/28 00:27:34
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("UE_LAN_1", "ടെക്സ്റ്റ് ബോക്സ്");
+define("UE_LAN_2", "റേഡിയോ ബട്ടണുകൾ");
+define("UE_LAN_3", "ഡ്രോപ്പ്-ഡൌൺ മെനു");
+define("UE_LAN_4", "ഡിബി പട്ടിക ഫീൽഡ്");
+define("UE_LAN_5", "ടെക്സ്റ്റ് ഏരിയ");
+define("UE_LAN_6", "പൂർണസംഖ്യ");
+define("UE_LAN_8", "ഭാഷ");
+define("UE_LAN_9", "മുൻനിർവ്വചിത ലിസ്റ്റ്");
+define("UE_LAN_10", "ചെക്ക്ബോക്സുകൾ");
+define("UE_LAN_13", "രാജ്യം");
+define("UE_LAN_21", "പേര്");
+define("UE_LAN_22", "തരം");
+define("UE_LAN_23", "ഉപയോഗം");
+define("UE_LAN_HIDE", "ഉപയോക്താക്കളിൽ നിന്ന് മറയ്ക്കുക");
+define("UE_LAN_LOCATION", "ലൊക്കേഷൻ");
+define("UE_LAN_LOCATION_DESC", "ഉപയോക്തൃ ലൊക്കേഷൻ");
+define("UE_LAN_AIM", "AIM വിലാസം");
+define("UE_LAN_AIM_DESC", "AIM വിലാസം");
+define("UE_LAN_ICQ", "ICQ നമ്പർ");
+define("UE_LAN_ICQ_DESC", "ICQ നമ്പർ");
+define("UE_LAN_YAHOO", "യാഹൂ വിലാസം");
+define("UE_LAN_YAHOO_DESC", "യാഹൂ വിലാസം");
+define("UE_LAN_MSN", "എം.എസ്.എൻ");
+define("UE_LAN_MSN_DESC", "എം.എസ്.എൻ വിലാസം");
+define("UE_LAN_HOMEPAGE", "ഹോംപേജ്");
+define("UE_LAN_HOMEPAGE_DESC", "ഉപയോക്താവ് ഹോംപേജ് (url)");
+define("UE_LAN_BIRTHDAY", "ജന്മദിനം");
+define("UE_LAN_BIRTHDAY_DESC", "ജന്മദിനം");
+define("UE_LAN_LANGUAGE", "ഭാഷ");
+define("UE_LAN_LANGUAGE_DESC", "ഉപയോക്തൃ ഭാഷ");
+define("UE_LAN_COUNTRY", "രാജ്യം");
+define("UE_LAN_COUNTRY_DESC", "ഉപയോക്തൃ രാജ്യം");
+define("UE_LAN_TIMEZONE", "സമയ മേഖല");
+define("UE_LAN_TIMEZONE_DESC", "ഉപയോക്താവ് സമയ മേഖല (മുൻനിർവ്വചിത ലിസ്റ്റിൽ നിന്ന്)");
+define("LAN_UE_FAIL_HOMEPAGE", "ഹോം പേജ് ക്രമീകരണത്തിനായി അസാധുവായ എൻട്രി");
+define("UE_LAN_SKYPE", "സ്കൈപ്പ്");
+define("UE_LAN_SKYPE_DESC", "സ്കൈപ്പ് വിലാസം");
+define("UE_LAN_GENDER", "ലിംഗഭേദം");
+define("UE_LAN_GENDER_DESC", "ലിംഗഭേദം");
+define("UE_LAN_MALE", "പുരുഷന്");
+define("UE_LAN_FEMALE", "സ്ത്രീ");
+define("UE_LAN_COMMENT", "അഭിപ്രായങ്ങൾ");
+define("UE_LAN_COMMENT_DESC", "അഭിപ്രായ ബോക്സ്");
+
+
+?>
