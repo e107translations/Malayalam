72dca74210d92c91653597a9973e165f19bfa96c
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-29 13:28:27 +0400

    Add e107_languages/Malayalam/lan_search.php

---
+++ e107_languages/Malayalam/lan_search.php
@@ -0,0 +1,104 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/27 23:26:11
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("PAGE_NAME", "തിരയുക");
+define("LAN_140", "അംഗങ്ങൾ");
+define("LAN_192", "എല്ലാ വിഭാഗങ്ങളും");
+define("LAN_193", "ഇവന്റ് കലണ്ടർ");
+define("LAN_194", "എല്ലാ വിഭാഗങ്ങളും");
+define("LAN_195", "തിരയുന്നു");
+define("LAN_196", "സമമായവ");
+define("LAN_197", "ഡൌൺലോഡുകൾ");
+define("LAN_198", "പൊരുത്തങ്ങളൊന്നും കണ്ടെത്തിയില്ല");
+define("LAN_199", "ഇതിനായി തിരയുക:");
+define("LAN_416", "ഈ പേജ് ആക്സസ് ചെയ്യുന്നതിന് നിങ്ങൾ പ്രവേശിച്ചിരിക്കണം");
+define("LAN_417", "തിരയൽ നിബന്ധനകൾ ചുരുങ്ങിയത് --CHARS-- അക്ഷരങ്ങൾ നീണ്ടതായിരിക്കണം.");
+define("LAN_418", "മറ്റ് പേജുകൾ");
+define("LAN_SEARCH_1", "എല്ലാം പരിശോധിക്കുക");
+define("LAN_SEARCH_2", "എല്ലാം അൺചെക്ക് ചെയ്യുക");
+define("LAN_SEARCH_3", "പോസ്റ്റ് ചെയ്തത്");
+define("LAN_SEARCH_4", "വാർത്താ തലവാചകത്തിൽ പൊരുത്തം കണ്ടെത്തി");
+define("LAN_SEARCH_5", "വാർത്താ വാചകത്തിൽ പൊരുത്തം കണ്ടെത്തുക");
+define("LAN_SEARCH_6", "വിപുലമായ വാർത്താ വാചകത്തിൽ പൊരുത്തം കണ്ടെത്തി");
+define("LAN_SEARCH_7", "പോസ്റ്റ് ചെയ്തത്");
+define("LAN_SEARCH_8", "ഇൽ");
+define("LAN_SEARCH_9", "ശീർഷകമില്ലാത്ത");
+define("LAN_SEARCH_11", "ഫലം");
+define("LAN_SEARCH_12", "അതിൽ");
+define("LAN_SEARCH_13", "അകത്ത്");
+define("LAN_SEARCH_14", "വർഗ്ഗം:");
+define("LAN_SEARCH_15", "രചയിതാവ്:");
+define("LAN_SEARCH_17", "ക്ഷമിക്കണം, നിയന്ത്രിതമാക്കിയിരിക്കുന്നു ഒരു തിരയൽ ഓരോ");
+define("LAN_SEARCH_18", "സെക്കൻഡുകളിലും എന്ന നിരക്കിൽ");
+define("LAN_SEARCH_19", "ഇതിൽ തിരയുക:");
+define("LAN_SEARCH_20", "അനുമതി ആവശ്യമാണ്");
+define("LAN_SEARCH_21", "ഈ പേജ് കാണുന്നതിന് നിങ്ങൾക്ക് അനുമതിയില്ല.");
+define("LAN_SEARCH_22", "എല്ലാ ഏരിയകളും");
+define("LAN_SEARCH_23", "മെച്ചപ്പെടുത്തിയ അന്വേഷണ ഫോം");
+define("LAN_SEARCH_24", "പദം(ങ്ങൾ) ഉണ്ടായിരിക്കണം");
+define("LAN_SEARCH_25", "വാക്ക് (കൾ) അടങ്ങിയിരിക്കരുത്");
+define("LAN_SEARCH_26", "കൃത്യമായ വാചകം");
+define("LAN_SEARCH_27", "പദം(ങ്ങൾ) പിന്പേപറയുന്ന പോലെ തുടങ്ങുന്നവ");
+define("LAN_SEARCH_28", "എല്ലാത്തിനും വിപുലമായ തിരയലില്ല");
+define("LAN_SEARCH_29", "അടിസ്ഥാനം");
+define("LAN_SEARCH_30", "വിപുലമായ");
+define("LAN_SEARCH_31", "വിപുലമായ തിരയൽ ലഭ്യമല്ല");
+define("LAN_SEARCH_32", "ഇനിപ്പറയുന്ന വാക്കുകൾ തിരയലിൽ നിന്ന് ഒഴിവാക്കി");
+define("LAN_SEARCH_33", "ഇനിപ്പറയുന്ന വാക്കുകൾ തിരയലിൽ നിന്ന് ഒഴിവാക്കി");
+define("LAN_SEARCH_34", "പുതിയത് ഇതിനേക്കാൾ");
+define("LAN_SEARCH_35", "പഴയത് ഇതിനേക്കാൾ");
+define("LAN_SEARCH_36", "ഏതുസമയത്തും");
+define("LAN_SEARCH_37", "ഒരു ദിവസം");
+define("LAN_SEARCH_38", "രണ്ടു ദിവസങ്ങൾ");
+define("LAN_SEARCH_39", "മൂന്ന് ദിവസങ്ങൾ");
+define("LAN_SEARCH_40", "ഒരു ആഴ്ച");
+define("LAN_SEARCH_41", "രണ്ടാഴ്ചകൾ");
+define("LAN_SEARCH_42", "മൂന്ന് ആഴ്ചകൾ");
+define("LAN_SEARCH_43", "ഒരു മാസം");
+define("LAN_SEARCH_44", "രണ്ടു മാസങ്ങൾ");
+define("LAN_SEARCH_45", "മൂന്നു മാസങ്ങൾ");
+define("LAN_SEARCH_46", "അര വർഷം");
+define("LAN_SEARCH_47", "ഒരു വർഷം");
+define("LAN_SEARCH_48", "രണ്ടു വർഷങ്ങൾ");
+define("LAN_SEARCH_49", "മൂന്നു വർഷങ്ങൾ");
+define("LAN_SEARCH_50", "പോസ്റ്റുചെയ്ത തീയതി");
+define("LAN_SEARCH_51", "എല്ലാ വിഭാഗങ്ങളും");
+define("LAN_SEARCH_52", "പൊരുപ്പെട്ടു ഇതിൽ");
+define("LAN_SEARCH_53", "മുഴുവൻ ഇനവും");
+define("LAN_SEARCH_54", "ടൈറ്റിൽ മാത്രം");
+define("LAN_SEARCH_55", "വാർത്താ വിഭാഗത്തിൽ തിരയുക");
+define("LAN_SEARCH_56", "എല്ലാ വാർത്താ വിഭാഗങ്ങളും");
+define("LAN_SEARCH_57", "പോസ്റ്റുചെയ്ത അഭിപ്രായങ്ങൾ");
+define("LAN_SEARCH_58", "എല്ലാ ഏരിയകളും");
+define("LAN_SEARCH_59", "എല്ലാ അഭിപ്രായങ്ങളും");
+define("LAN_SEARCH_60", "പോസ്റ്റുചെയ്ത അഭിപ്രായങ്ങൾ");
+define("LAN_SEARCH_61", "രചയിതാവിന്റേത്");
+define("LAN_SEARCH_62", "ചേർന്ന തീയതി");
+define("LAN_SEARCH_63", "വിഭാഗത്തിൽ തിരയുക");
+define("LAN_SEARCH_64", "എല്ലാ ഡൌൺലോഡ് വിഭാഗങ്ങളും");
+define("LAN_SEARCH_65", "ഡൗൺലോഡുകൾ");
+define("LAN_SEARCH_66", "ചേർത്ത തീയതി");
+define("LAN_SEARCH_67", "എല്ലാ ഡൗൺലോഡുകളും വിശദാംശങ്ങൾ");
+define("LAN_SEARCH_69", "പ്രാധാന്യം");
+define("LAN_SEARCH_70", "ഡൗൺലോഡ് ഇനത്തിൽ പോസ്റ്റുചെയ്തു");
+define("LAN_SEARCH_71", "വാർത്താ ഇനത്തിൽ പോസ്റ്റ് ചെയ്തിരിക്കുന്നു");
+define("LAN_SEARCH_72", "സിഗ്നേച്ചർ");
+define("LAN_SEARCH_73", "സിഗ്നേച്ചർ ഇല്ല ");
+define("LAN_SEARCH_74", "ചേർന്നു");
+define("LAN_SEARCH_75", "തിരയൽ തരം");
+define("LAN_SEARCH_76", "പേജിൽ പോസ്റ്റ് ചെയ്തിരിക്കുന്നു ");
+define("LAN_SEARCH_77", "പിൻ പ്രൊഫൈൽ പേജിൽ പോസ്റ്റ് ചെയ്തിരിക്കുന്നു");
+define("LAN_SEARCH_98", "വാർത്ത");
+define("LAN_SEARCH_201", "നിങ്ങളുടെ തിരയൽ അന്വേഷണം പുനർനിര്‍വചിക്കുക");
+define("LAN_SEARCH_202", "വിപുലമായ മോഡ് ടോഗിൾ ചെയ്യുക");
+
+
+?>
