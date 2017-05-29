04befc7dc6b7ee1ba43ac540af0e4841b68855ca
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-29 10:22:41 +0400

    Add e107_languages/Malayalam/lan_library_manager.php

---
+++ e107_languages/Malayalam/lan_library_manager.php
@@ -0,0 +1,34 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 06:19:36
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_LIBRARY_MANAGER_01", "[y] ലൈബ്രറി ആശ്രയിച്ചിരിക്കുന്ന [x] ലൈബ്രറി, ഇൻസ്റ്റാൾ ചെയ്തിട്ടില്ല.");
+define("LAN_LIBRARY_MANAGER_02", "[y] ലൈബ്രറിയുടെ പതിപ്പ് [x] [z] ലൈബ്രറിയുമായി പൊരുത്തപ്പെടുന്നില്ല.");
+define("LAN_LIBRARY_MANAGER_03", "[x] ലൈബ്രറി കണ്ടെത്താനായില്ല.");
+define("LAN_LIBRARY_MANAGER_04", "[x] ലൈബ്രറിയുടെ പതിപ്പ് കണ്ടെത്താനായില്ല.");
+define("LAN_LIBRARY_MANAGER_05", "[y] ലൈബ്രറിയുടെ ഇൻസ്റ്റോൾ ചെയ്ത പതിപ്പ് [x] നെ പിന്തുണയ്ക്ക്കാനാവില്ല.");
+define("LAN_LIBRARY_MANAGER_06", "[y] ലൈബ്രറിയുടെ [x] വേരിയന്റ് കണ്ടെത്താൻ കഴിഞ്ഞില്ല.");
+define("LAN_LIBRARY_MANAGER_07", "കണ്ടെത്താത്താനാവാത്ത ആശ്രയത്വം");
+define("LAN_LIBRARY_MANAGER_08", "അനുയോജ്യമല്ലാത്ത ആശ്രയത്വം");
+define("LAN_LIBRARY_MANAGER_10", "കണ്ടെത്തിയില്ല");
+define("LAN_LIBRARY_MANAGER_11", "പിന്തുണയ്ക്കുന്നില്ല");
+define("LAN_LIBRARY_MANAGER_13", "ലൈബ്രറി");
+define("LAN_LIBRARY_MANAGER_21", "ദാതാവ്");
+define("LAN_LIBRARY_MANAGER_25", "മൂന്നാം-കക്ഷി ലൈബ്രറികൾ");
+define("LAN_LIBRARY_MANAGER_27", "മെഷീൻ പേര്: [x]");
+define("LAN_LIBRARY_MANAGER_28", "ലൈബ്രറി പാത: [x]");
+define("LAN_LIBRARY_MANAGER_29", "ലൈബ്രറി പാത");
+define("LAN_LIBRARY_MANAGER_30", "CDN ക്രമീകരണങ്ങൾ");
+define("LAN_LIBRARY_MANAGER_31", "കോർ ലൈബ്രറികൾക്കായി CDN ഉപയോഗിക്കുക");
+define("LAN_LIBRARY_MANAGER_32", "CDN പ്രൊവൈഡർ");
+
+
+?>
