cb5f12f21b4b5d0a56eade0883ee5eab01f52340
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-06-02 20:57:43 +0400

    Add e107_themes/bootstrap3/languages/Malayalam_admin.php

---
+++ e107_themes/bootstrap3/languages/Malayalam_admin.php
@@ -0,0 +1,25 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/26 09:56:12
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("LAN_THEMEPREF_00", "ബ്രാൻഡിംഗ്:");
+define("LAN_THEMEPREF_01", "നാവ്‌ബാർ വിന്യാസം:");
+define("LAN_THEMEPREF_02", "സൈൻഅപ്പ് / ലോഗിൻ സ്ഥാനം:");
+define("LAN_THEMEPREF_03", "Bootswatch സ്റ്റൈലുകൾ:");
+define("LAN_THEMEPREF_04", "സൈറ്റിന്റെ പേര്");
+define("LAN_THEMEPREF_05", "ലോഗോ");
+define("LAN_THEMEPREF_06", "ലോഗോ & സൈറ്റിന്റെ പേര്");
+define("LAN_THEMEPREF_07", "ഇടത്");
+define("LAN_THEMEPREF_08", "വലത്");
+define("LAN_THEMEPREF_09", "മുകളിൽ");
+define("LAN_THEMEPREF_10", "താഴെ");
+
+
+?>
