422d6006f9e2b9135315b0e54826e706513ad7c8
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-06-05 13:39:18 +0400

    Add e107_languages/Malayalam/admin/lan_meta.php

---
+++ e107_languages/Malayalam/admin/lan_meta.php
@@ -0,0 +1,20 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/05 09:38:01
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("METLAN_00", "മെറ്റാ ടാഗുകൾ");
+define("METLAN_1", "കൂടുതൽ മെറ്റാ ടാഗുകൾ");
+define("METLAN_2", "ഉദാ. < meta name='revisit-after' content='30 days' />
+");
+define("METLAN_3", "വാർത്ത പേജുകളിലെ മെറ്റാ-വിവരണം എന്ന രീതിയിൽ വാർത്താ തലക്കെട്ടവും സംഗ്രഹവും ഉപയോഗിക്കുക.");
+
+
+?>
