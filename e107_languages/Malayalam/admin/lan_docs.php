2d64eb0c2ea52dc3c808c6f393298832a6f85803
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-06-05 20:10:30 +0400

    Add e107_languages/Malayalam/admin/lan_docs.php

---
+++ e107_languages/Malayalam/admin/lan_docs.php
@@ -0,0 +1,20 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/05 16:09:51
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_DOCS", "സിസ്റ്റം ഡോക്സ്");
+define("LAN_DOCS_SECTIONS", "വിഭാഗങ്ങൾ");
+define("LAN_DOCS_GOTOP", "മുകളിലേക്ക് പോകുക");
+define("LAN_DOCS_ANSWER", "ഉത്തരം");
+define("LAN_DOCS_QUESTION", "ചോദ്യം");
+
+
+?>
