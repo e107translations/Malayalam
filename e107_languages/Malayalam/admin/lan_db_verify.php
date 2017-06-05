bafd39dab584b6270abc76bc71b45f35ba90bb32
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-06-05 21:29:15 +0400

    Add e107_languages/Malayalam/admin/lan_db_verify.php

---
+++ e107_languages/Malayalam/admin/lan_db_verify.php
@@ -0,0 +1,35 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/05 17:28:09
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("DBVLAN_1", "sql ഡാറ്റാഫയല് വായിക്കാനായില്ല<br /><br /> <b>core_sql.php</b> ഫയൽ <b>/e107_core/sql</b> ഡയറക്ടറിയിൽ ഉണ്ടെന്ന് ഉറപ്പാക്കുക.");
+define("DBVLAN_4", "ടേബിൾ");
+define("DBVLAN_5", "ഫീൽഡ്");
+define("DBVLAN_6", "സ്ഥിതി");
+define("DBVLAN_7", "കുറിപ്പുകൾ");
+define("DBVLAN_8", "പൊരുത്തക്കേട്");
+define("DBVLAN_9", "നിലവിൽ");
+define("DBVLAN_10", "ആയിരിക്കണം");
+define("DBVLAN_11", "ഫീൽഡ് കാണുന്നില്ല!");
+define("DBVLAN_12", "അധിക ഫീൽഡ്!");
+define("DBVLAN_13", "ടേബിൾ കാണുന്നില്ല!");
+define("DBVLAN_14", "മൂല്യനിർണ്ണയം ചെയ്യാനുള്ള ടേബിൾ(കൾ) തിരഞ്ഞെടുക്കുക");
+define("DBVLAN_15", "പരിശോധന ആരംഭിക്കുക");
+define("DBVLAN_16", "SQL വെരിഫിക്കേഷൻ");
+define("DBVLAN_19", "പരിഹരിക്കാൻ ശ്രമിക്കുക");
+define("DBVLAN_21", "തിരഞ്ഞെടുത്ത ഇനങ്ങൾ പരിഹരിക്കുക");
+define("DBVLAN_22", "[x] വായിക്കാനാവില്ല");
+define("DBVLAN_23", "ഡാറ്റാബേസ് യൂട്ടിലിറ്റികൾ");
+define("DBVLAN_24", "ദയവായി പ്രവർത്തനം തിരഞ്ഞെടുക്കുക.");
+define("DBVLAN_25", "ഇൻഡെക്സ് നഷ്ടമായി!");
+define("DBVLAN_26", "[x] പട്ടിക(കൾ) ഇൽ പ്രശ്നങ്ങളുണ്ട്.");
+
+
+?>
