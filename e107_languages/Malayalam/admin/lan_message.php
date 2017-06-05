6914cc02cdf653fca579306b78252ff6fcfab924
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-06-05 13:36:02 +0400

    Add e107_languages/Malayalam/admin/lan_message.php

---
+++ e107_languages/Malayalam/admin/lan_message.php
@@ -0,0 +1,28 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/05 09:34:50
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("MESSLAN_1", "സ്വീകരിച്ച സന്ദേശങ്ങൾ");
+define("MESSLAN_2", "സന്ദേശം നീക്കം ചെയ്യുക");
+define("MESSLAN_3", "സന്ദേശം നീക്കം ചെയ്യപ്പെട്ടു");
+define("MESSLAN_4", "എല്ലാ സന്ദേശങ്ങളും നീക്കം ചെയ്യുക");
+define("MESSLAN_5", "ഉറപ്പിക്കുക");
+define("MESSLAN_6", "എല്ലാ സന്ദേശങ്ങളും നീക്കം ചെയ്തു.");
+define("MESSLAN_7", "സന്ദേശങ്ങളൊന്നുമില്ല.");
+define("MESSLAN_8", "സന്ദേശ തരം");
+define("MESSLAN_9", "റിപ്പോർട്ട് ചെയ്യപ്പെട്ടത്");
+define("MESSLAN_10", "സമർപ്പിച്ചത്");
+define("MESSLAN_11", "പുതിയ വിൻഡോയിൽ തുറക്കുന്നു");
+define("MESSLAN_12", "സന്ദേശം");
+define("MESSLAN_13", "ലിങ്ക്");
+
+
+?>
