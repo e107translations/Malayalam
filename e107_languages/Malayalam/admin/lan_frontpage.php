1ad0aabfddf6db13dc63c09a46834316af46e980
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-06-05 20:27:58 +0400

    Add e107_languages/Malayalam/admin/lan_frontpage.php

---
+++ e107_languages/Malayalam/admin/lan_frontpage.php
@@ -0,0 +1,27 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/05 16:27:28
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("FRTLAN_13", "നിലവിലെ ഫ്രണ്ട് പേജ് ക്രമീകരണങ്ങൾ");
+define("FRTLAN_30", "കസ്റ്റം പേജ്");
+define("FRTLAN_35", "പോസ്റ്റ്-ലോഗിൻ പേജ്");
+define("FRTLAN_42", "പുതിയ നിയമം ചേർക്കുക");
+define("FRTLAN_43", "ക്ലാസ്");
+define("FRTLAN_46", "നിലവിലുള്ള നിയമം തിരുത്തുക");
+define("FRTLAN_49", "ഹോം പേജ്");
+define("FRTLAN_51", "മറ്റുള്ളവ");
+define("FRTLAN_PAGE_TITLE", "ഫ്രന്റ് പേജ്");
+define("FRTLAN_56", "ക്ലാസിന്റെ തനിപ്പകർപ്പ് നിർവചനം:");
+define("FRTLAN_57", "സോഫ്റ്റ്വെയർ പിശക്");
+define("FRTLAN_61", "തിരഞ്ഞെടുപ്പ്");
+
+
+?>
