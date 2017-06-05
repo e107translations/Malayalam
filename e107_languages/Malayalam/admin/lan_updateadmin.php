5944250ef6fe6adf8fad702a552d262065f4beb3
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-06-05 20:12:39 +0400

    Add e107_languages/Malayalam/admin/lan_updateadmin.php

---
+++ e107_languages/Malayalam/admin/lan_updateadmin.php
@@ -0,0 +1,22 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/05 16:12:11
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("UDALAN_1", "പിശക് - ദയവായി വീണ്ടും സമർപ്പിക്കുക.");
+define("UDALAN_2", "അഡ്മിൻ ക്രമീകരണം അപ്ഡേറ്റുചെയ്തു");
+define("UDALAN_3", "അപ്ഡേറ്റുചെയ്ത ക്രമീകരണങ്ങൾ ഇതിനായി");
+define("UDALAN_4", "പേര്");
+define("UDALAN_6", "പാസ്വേഡ് വീണ്ടും ടൈപ്പുചെയ്യുക");
+define("UDALAN_7", "പാസ്വേഡ് മാറ്റുക");
+define("UDALAN_8", "പാസ്വേഡ് അപ്ഡേറ്റുചെയ്തു");
+
+
+?>
