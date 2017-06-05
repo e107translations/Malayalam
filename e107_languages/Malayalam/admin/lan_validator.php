7e859260c1ec2f6c2483e5e2333c7d22617eac7e
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-06-06 00:14:30 +0400

    Add e107_languages/Malayalam/admin/lan_validator.php

---
+++ e107_languages/Malayalam/admin/lan_validator.php
@@ -0,0 +1,36 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/05 20:09:39
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("LAN_VALIDATE_0", "അജ്ഞാത പിശക്");
+define("LAN_VALIDATE_101", "മൂല്യം നഷ്ടമായിരിക്കുന്നു");
+define("LAN_VALIDATE_102", "അപ്രതീക്ഷിത മൂല്യ തരം");
+define("LAN_VALIDATE_103", "അസാധുവായ പ്രതീകങ്ങൾ കണ്ടെത്തി");
+define("LAN_VALIDATE_104", "സാധുവായ ഒരു ഇമെയിൽ വിലാസമല്ല");
+define("LAN_VALIDATE_105", "ഫീൽഡുകൾ പൊരുത്തമില്ലാത്തവ");
+define("LAN_VALIDATE_131", "സ്‌ട്രിങ് വളരെ ചെറുതാണ്");
+define("LAN_VALIDATE_132", "സ്‌ട്രിങ് ദൈർഘ്യമേറിയതാണ്");
+define("LAN_VALIDATE_133", "നന്പർ വളരെ കുറവാണ്");
+define("LAN_VALIDATE_134", "നന്പർ വളരെ ഉയർന്നതാണ്");
+define("LAN_VALIDATE_135", "അറേ എണ്ണം വളരെ കുറവാണ്");
+define("LAN_VALIDATE_136", "അറേ എണ്ണം വളരെ കൂടുതലാണ്");
+define("LAN_VALIDATE_151", "പ്രതീക്ഷിക്കുന്ന തരം പൂർണ്ണസംഖ്യകളുടെ എണ്ണം");
+define("LAN_VALIDATE_152", "പ്രതീക്ഷിക്കുന്ന തരം ഫ്ലോട്ട് സംഖ്യകളുടെ എണ്ണം");
+define("LAN_VALIDATE_153", "ഇൻസ്റ്റൻസ് തരം പ്രതീക്ഷിക്കുന്നു");
+define("LAN_VALIDATE_154", "അറേ തരം പ്രതീക്ഷിക്കുന്നു");
+define("LAN_VALIDATE_191", "ശൂന്യമായ മൂല്യം");
+define("LAN_VALIDATE_201", "ഫയൽ നിലവിലില്ല");
+define("LAN_VALIDATE_202", "ഫയൽ എഴുതാൻ കഴിയുന്നില്ല");
+define("LAN_VALIDATE_203", " ഫയൽ വലുപ്പം അനുവദനീയമായ ഫയൽ വലുപത്തിലും അധികമാണ്");
+define("LAN_VALIDATE_204", "ഫയൽ വലുപ്പം അനുവദനീയമായ കുറഞ്ഞ ഫയൽ വലുപ്പത്തിലും കുറവാണ്");
+define("LAN_VALIDATE_FAILMSG", "[x] മൂല്യനിർണ്ണയ പിശക്: [y] [z].");
+
+
+?>
