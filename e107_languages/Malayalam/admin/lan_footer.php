2d94e6c6f896a568e9ca908bb46d5326ce572bde
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-06-05 14:12:17 +0400

    Add  e107_languages/Malayalam/admin/lan_footer.php

---
+++ e107_languages/Malayalam/admin/lan_footer.php
@@ -0,0 +1,35 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/05 10:11:42
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("FOOTLAN_1", "സൈറ്റ്");
+define("FOOTLAN_2", "ഹെഡ് അഡ്മിൻ");
+define("FOOTLAN_3", "പതിപ്പ്");
+define("FOOTLAN_4", "നിർമ്മിതി");
+define("FOOTLAN_5", "അഡ്മിൻ തീം");
+define("FOOTLAN_6", "വഴി");
+define("FOOTLAN_7", "വിവരം");
+define("FOOTLAN_8", "ഇൻസ്റ്റാൾ ചെയ്ത തീയതി");
+define("FOOTLAN_9", "സെർവർ");
+define("FOOTLAN_10", "ഹോസ്റ്റ്");
+define("FOOTLAN_11", "PHP പതിപ്പ്");
+define("FOOTLAN_12", "MySQL");
+define("FOOTLAN_13", "സൈറ്റ് വിവരം");
+define("FOOTLAN_14", "ഡോക്സ് കാണിക്കുക");
+define("FOOTLAN_15", "ഡോക്യുമെന്റേഷൻ");
+define("FOOTLAN_16", "ഡാറ്റാബേസ്");
+define("FOOTLAN_17", "ചാർസെറ്റ്");
+define("FOOTLAN_18", "സൈറ്റ് തീം");
+define("FOOTLAN_19", "സെർവർ സമയം");
+define("FOOTLAN_20", "സുരക്ഷാ നില");
+
+
+?>
