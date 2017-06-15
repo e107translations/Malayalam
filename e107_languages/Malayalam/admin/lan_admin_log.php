0ff2a8529d7577fe552170d13dfb404073a4fa2d
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-06-15 13:53:26 +0400

    Add e107_languages/Malayalam/admin/lan_admin_log.php

---
+++ e107_languages/Malayalam/admin/lan_admin_log.php
@@ -0,0 +1,106 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/15 09:51:48
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("RL_LAN_002", "റോളിംഗ് ലോഗ്");
+define("RL_LAN_005", "സിസ്റ്റം ലോഗുകൾ കോൺഫിഗർ ചെയ്യുക/കാണുക");
+define("RL_LAN_008", "റോളിംഗ് ലോഗ് സജീവമാണ്:");
+define("RL_LAN_009", "റോളിംഗ് ലോംഗ് ചരിത്ര ദൈർഘ്യം ദിവസങ്ങളിൽ");
+define("RL_LAN_012", "ഫിൽട്ടർ ഓപ്ഷനുകൾ");
+define("RL_LAN_013", "ആരംഭിക്കുന്ന തീയതി/സമയം");
+define("RL_LAN_014", "");
+define("RL_LAN_015", "");
+define("RL_LAN_016", "");
+define("RL_LAN_017", "");
+define("RL_LAN_018", "");
+define("RL_LAN_019", "");
+define("RL_LAN_020", "");
+define("RL_LAN_023", "");
+define("RL_LAN_024", "");
+define("RL_LAN_025", "");
+define("RL_LAN_026", "");
+define("RL_LAN_028", "");
+define("RL_LAN_029", "");
+define("RL_LAN_030", "");
+define("RL_LAN_031", "");
+define("RL_LAN_032", "");
+define("RL_LAN_033", "");
+define("RL_LAN_044", "");
+define("RL_LAN_045", "");
+define("RL_LAN_046", "");
+define("RL_LAN_047", "");
+define("RL_LAN_049", "");
+define("RL_LAN_050", "");
+define("RL_LAN_052", "");
+define("RL_LAN_053", "");
+define("RL_LAN_054", "");
+define("RL_LAN_057", "");
+define("RL_LAN_058", "");
+define("RL_LAN_059", "");
+define("RL_LAN_060", "");
+define("RL_LAN_061", "");
+define("RL_LAN_062", "");
+define("RL_LAN_064", "");
+define("RL_LAN_065", "");
+define("RL_LAN_066", "");
+define("RL_LAN_067", "");
+define("RL_LAN_068", "");
+define("RL_LAN_069", "");
+define("RL_LAN_071", "");
+define("RL_LAN_072", "");
+define("RL_LAN_073", "");
+define("RL_LAN_075", "");
+define("RL_LAN_076", "");
+define("RL_LAN_077", "");
+define("RL_LAN_078", "");
+define("RL_LAN_079", "");
+define("RL_LAN_080", "");
+define("RL_LAN_081", "");
+define("RL_LAN_082", "");
+define("RL_LAN_083", "");
+define("RL_LAN_084", "");
+define("RL_LAN_087", "");
+define("RL_LAN_090", "");
+define("RL_LAN_091", "");
+define("RL_LAN_092", "");
+define("RL_LAN_093", "");
+define("RL_LAN_094", "");
+define("RL_LAN_096", "");
+define("RL_LAN_100", "");
+define("RL_LAN_101", "");
+define("RL_LAN_103", "");
+define("RL_LAN_104", "");
+define("RL_LAN_106", "");
+define("RL_LAN_107", "");
+define("RL_LAN_108", "");
+define("RL_LAN_109", "");
+define("RL_LAN_110", "");
+define("RL_LAN_111", "");
+define("RL_LAN_112", "");
+define("RL_LAN_113", "");
+define("RL_LAN_114", "");
+define("RL_LAN_115", "");
+define("RL_LAN_116", "");
+define("RL_LAN_117", "");
+define("RL_LAN_118", "");
+define("RL_LAN_120", "");
+define("RL_LAN_122", "");
+define("RL_LAN_123", "");
+define("RL_LAN_124", "");
+define("RL_LAN_125", "");
+define("RL_LAN_126", "");
+define("RL_LAN_132", "");
+define("RL_LAN_133", "");
+define("RL_LAN_134", "");
+define("RL_LAN_135", "");
+
+
+?>
