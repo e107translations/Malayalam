217697417b9e308ef1b5ebd39647b551bc118fcd
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-08-01 13:24:59 +0400

    Add e107_languages/Malayalam/admin/lan_cpage.php

---
+++ e107_languages/Malayalam/admin/lan_cpage.php
@@ -0,0 +1,57 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/08/01 09:24:08
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("CUSLAN_1", "അവലോകനം");
+define("CUSLAN_2", "പേജ് തലക്കെട്ട്");
+define("CUSLAN_3", "ഓരോ പേജിലുമുള്ള ഇനങ്ങൾ");
+define("CUSLAN_4", "കസ്റ്റം ഫീൽഡുകൾ");
+define("CUSLAN_5", "(പുതിയ പുസ്തകം)");
+define("CUSLAN_9", "വാചകം");
+define("CUSLAN_11", "മെറ്റാ വിവരണം");
+define("CUSLAN_12", "പേജ്/മെനു സൃഷ്ടിക്കുക");
+define("CUSLAN_29", "പേജൊന്നും തിരഞ്ഞെടുത്തില്ലെങ്കിൽ പേജുകൾ നിരത്തുക");
+define("CUSLAN_30", "കുക്കി കാലഹരണപ്പെടുന്ന സമയം (നിമിഷങ്ങളിൽ)");
+define("CUSLAN_31", "മെനു സൃഷ്ടിക്കുക");
+define("CUSLAN_48", "പേജ് പട്ടിക");
+define("CUSLAN_49", "മെനു പട്ടിക");
+define("CUSLAN_50", "പുസ്തകങ്ങൾ/അദ്ധ്യായങ്ങൾ നിരത്തുക ");
+define("CUSLAN_51", "പുസ്തകം/അദ്ധ്യായം ചേർക്കുക");
+define("CUSLAN_52", "പുസ്തകം");
+define("CUSLAN_53", "പുസ്തക അല്ലെങ്കിൽ അദ്ധ്യായ ശീർഷകം");
+define("CUSLAN_55", "എഡിറ്റുചെയ്യാൻ കഴിയും");
+define("CUSLAN_56", "പാരന്റ്");
+define("CUSLAN_57", "");
+define("CUSLAN_58", "");
+define("CUSLAN_59", "");
+define("CUSLAN_60", "");
+define("CUSLAN_61", "");
+define("CUSLAN_62", "");
+define("CUSLAN_63", "");
+define("CUSLAN_64", "");
+define("CUSLAN_65", "");
+define("CUSLAN_66", "");
+define("CUSLAN_67", "");
+define("CUSLAN_68", "");
+define("CUSLAN_69", "");
+define("CUSLAN_70", "");
+define("CUSLAN_71", "");
+define("CUSLAN_72", "");
+define("CUSLAN_73", "");
+define("CUSLAN_74", "");
+define("CUSLAN_75", "");
+define("CUSLAN_76", "");
+define("CUSLAN_77", "");
+define("CUSLAN_78", "");
+define("CUSLAN_79", "");
+
+
+?>
