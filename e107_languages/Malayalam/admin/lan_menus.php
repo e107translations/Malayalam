e1aedf7657b4dc7b9ac540ad335a0935f0c74822
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-08-01 18:07:45 +0400

    Correct wrong previous commit.

--- e107_languages/Malayalam/admin/lan_menus.php
+++ e107_languages/Malayalam/admin/lan_menus.php
@@ -1,83 +1,57 @@
 <?php
 /*
- * Copyright (C) 2008-2016 e107 Inc (e107.org), Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
- *
- * Admin Language File
- *
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/08/01 14:03:03
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
 */
-
-//define("MENLAN_1", "Visible to all");
-//define("MENLAN_2", "Visible to members only");
-//define("MENLAN_3", "Visible to administrators only");
-//define("MENLAN_4", "Only visible to:");//LAN_VISIBLE_TO
-// define("MENLAN_5", "class");
-//define("MENLAN_6", "Save visibility options");
-define("MENLAN_7", "Configure visibility options for");
-//define("MENLAN_8", "Visibility options updated");
-//define("MENLAN_9", "New custom menu installed");
-define("MENLAN_10", "New menu installed");
-define("MENLAN_11", "Menu removed");
-//define("MENLAN_12", "Activate: choose area");
-define("MENLAN_13", "Activate in Area");
-define("MENLAN_14", "Area");
-define("MENLAN_15", "Deactivate");
-//define("MENLAN_16", "Configure");    // now in lan_admin.php
-define("MENLAN_17", "Move Up");
-define("MENLAN_18", "Move Down");
-define("MENLAN_19", "Move to Area");
-//define("MENLAN_20", "Visibility");//LAN_VISIBILITY
-
-// define("MENLAN_21", "Visible to Guests only");
-define("MENLAN_22", "Inactive Menus");
-
-define("MENLAN_23", "Move to bottom");
-define("MENLAN_24", "Move to top");
-define("MENLAN_25", "Action...");
-
-define("MENLAN_26", "This menu will only be [b]SHOWN[/b] on the following pages");
-define("MENLAN_27", "This menu will only be [b]HIDDEN[/b] on the following pages");
-define("MENLAN_28", "Enter one page per line, enter enough of the url to distinguish it properly.  If you need the ending of the url to match exactly, use a ! at the end of the page name. For example: [b]page.php?1![/b]");
-
-//define("MENLAN_29", "Select Layout");
-define("MENLAN_30", "To see the menu areas and their positions for custom layouts, select the custom layout here.");
-define("MENLAN_31", "Default Layout");
-//define("MENLAN_32", "Newsheader Layout");
-define("MENLAN_33", "Custom Layout");
-define("MENLAN_34", "Embedded");
-//define("MENLAN_35", "Configure Menus");    now in lan_admin.php
-define("MENLAN_36", "Choose the menu(s) to activate");
-define("MENLAN_37", "and where to activate them.");
-//define("MENLAN_38", "Hold down CTRL to select multiple menus.");
-
-// --
-
-define("MENLAN_39", "Preset Area");
-define("MENLAN_40", "Use Menu Presets");
-define("MENLAN_41", "The position of all your menus for this layout will be lost. Do you still wish to continue?");
-//define("MENLAN_42", "Custom");//LAN_CUSTOM
-define("MENLAN_43", "Menu Preset Activated");
-
-define("MENLAN_44", "Menu parameters");
-define("MENLAN_45", "Parameters (query string format):");
-
-define("MENLAN_46", "[x] object not found. Try re-scanning plugin directories in Tools > Database.");
-define("MENLAN_47", "No Fields Set in");
-define("MENLAN_48", "Menu could not be loaded");
-define("MENLAN_49", "Your Menus");
-define("MENLAN_50", "Plugin Menus");
-define("MENLAN_51", "This layout does NOT contain any dynamic {MENU} areas.");
-define("MENLAN_52", "It DOES contain the following custom menus: ");
-define("MENLAN_53", "Go to Custom-Menu Area");
-define("MENLAN_54", "Theme Layout");
-define("MENLAN_55", "Menu Layout");
-define("MENLAN_56", "Custom Pages");
-define("MENLAN_57", "Drag-and-Drop Menus");
-define("MENLAN_58", "The Menu-Manager allows you to place and arrange your menus within your theme template. Hover over the sub-areas to modify existing menu items.");
-
-define("MENLAN_59", "Area [x]");
-define("MENLAN_60", "This theme is using deprecated elements. All [x]HEADER and [x]FOOTER variables should be removed from theme.php.");
-
-
+define("MENLAN_7", "ദൃശ്യപരത ഓപ്ഷനുകൾ കോൺഫിഗർ ചെയ്യുക");
+define("MENLAN_10", "പുതിയ മെനു ഇൻസ്റ്റാളുചെയ്തു");
+define("MENLAN_11", "മെനു നീക്കം ചെയ്തു");
+define("MENLAN_13", "ഏരിയയിൽ സജീവമാക്കുക");
+define("MENLAN_14", "ഏരിയ");
+define("MENLAN_15", "നിർജ്ജീവമാക്കുക");
+define("MENLAN_17", "മുകളിലേക്ക് നീക്കുക");
+define("MENLAN_18", "താഴേക്ക് നീക്കുക");
+define("MENLAN_19", "ഏരിയയിലേക്ക് നീക്കുക");
+define("MENLAN_22", "നിർജ്ജീവമായ മെനുകൾ");
+define("MENLAN_23", "അടിത്തട്ടിലേക്കു നീക്കുക");
+define("MENLAN_24", "മുടിത്തട്ടിലേക്കു നീക്കുക");
+define("MENLAN_25", "പ്രവർത്തനം...");
+define("MENLAN_26", "ഈ മെനു താഴെപറയുന്ന പേജുകളില്മാത്രമേ  [b]കാണിക്കുകയുള്ളൂ[/ b]");
+define("MENLAN_27", "");
+define("MENLAN_28", "");
+define("MENLAN_30", "");
+define("MENLAN_31", "");
+define("MENLAN_33", "");
+define("MENLAN_34", "");
+define("MENLAN_36", "");
+define("MENLAN_37", "");
+define("MENLAN_39", "");
+define("MENLAN_40", "");
+define("MENLAN_41", "");
+define("MENLAN_43", "");
+define("MENLAN_44", "");
+define("MENLAN_45", "");
+define("MENLAN_46", "");
+define("MENLAN_47", "");
+define("MENLAN_48", "");
+define("MENLAN_49", "");
+define("MENLAN_50", "");
+define("MENLAN_51", "");
+define("MENLAN_52", "");
+define("MENLAN_53", "");
+define("MENLAN_54", "");
+define("MENLAN_55", "");
+define("MENLAN_56", "");
+define("MENLAN_57", "");
+define("MENLAN_58", "");
+define("MENLAN_59", "");
+define("MENLAN_60", "");
 
 
 ?>
