dec8ecb942eae7d7a3d97bcebaa541634ee86e6b
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-06 00:02:35 +0400

    Add admin links terms - partial

---
+++ e107_languages/Malayalam/admin/lan_links.php
@@ -0,0 +1,38 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/05 20:01:15
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LCLAN_19", "ലിങ്ക് തുറക്കുന്ന തരം");
+define("LCLAN_20", "ഒരേ വിൻഡോയിൽ തുറക്കുന്നു");
+define("LCLAN_23", "പുതിയ വിൻഡോയിൽ തുറക്കുന്നു");
+define("LCLAN_24", "600x400 മിനി-വിൻഡോയിൽ തുറക്കുന്നു");
+define("LCLAN_78", "വിവരണം സ്ക്രീൻ-ടിപ്പ് ആയി കാണിക്കുക");
+define("LCLAN_79", "മൗസ്  ലിങ്കിനു മുകളിൽ ഹോവർ ചെയ്യുമ്പോൾ വിവരണം പ്രദർശിപ്പിക്കും");
+define("LCLAN_80", "വികസിക്കുന്ന തരം സബ് മെനുകൾ സജീവമാക്കുക");
+define("LCLAN_81", "ഉപ-മെനുകൾ അവരുടെ പാരന്റ് ലിങ്കുകൾ ക്ലിക്കുചെയ്തതിനുശേഷം മാത്രമേ പ്രദർശിപ്പിക്കൂ. (ലിങ്ക് പാരന്റ് അപ്രാപ്തമാക്കിയിരിക്കും)");
+define("LCLAN_105", "");
+define("LCLAN_106", "");
+define("LCLAN_107", "");
+define("LCLAN_108", "");
+define("LCLAN_109", "");
+define("LCLAN_110", "");
+define("LCLAN_111", "");
+define("LINKLAN_1", "");
+define("LINKLAN_4", "");
+define("LINKLAN_5", "");
+define("LINKLAN_6", "");
+define("LINKLAN_7", "");
+define("LINKLAN_8", "");
+define("LINKLAN_9", "");
+define("LINKLAN_10", "");
+
+
+?>
