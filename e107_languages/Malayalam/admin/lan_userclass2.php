bdb1180cd0bd9f89273cd62df95bb2d18f7114bd
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-07 01:06:31 +0400

    Add userclass2 terms - partial

---
+++ e107_languages/Malayalam/admin/lan_userclass2.php
@@ -0,0 +1,62 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/06 20:46:50
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("UCSLAN_10", "ക്ലാസ് ഇല്ലാതാക്കിയിട്ടില്ല - ഇത് ഒരു കോർ ക്ലാസ് ആണ്, അല്ലെങ്കിൽ അത് മറ്റൊരു ക്ലാസ്സ് നിർവ്വചനത്തിനായി ഉപയോഗിച്ചിട്ടുള്ളതാണ്, അതിന്റെ പാരന്റ് ക്ലാസ്സോ  അല്ലെങ്കിൽ എഡിറ്റ് ക്ലാസ്സോ ആയി.");
+define("UCSLAN_30", "സെലക്ടറുകളിൽ പ്രദർശിപ്പിച്ചിരിക്കുന്ന ഹ്രസ്വനാമം");
+define("UCSLAN_31", "ക്ലാസ് പ്രയോഗക്ഷമത സംബന്ധിച്ച വിവരങ്ങൾ");
+define("UCSLAN_32", "ഈ ക്ലാസ്സിലെ ഉപയോക്താക്കൾക്ക് എഡിറ്റുചെയ്യുന്ന ക്ലാസിൽ നിന്നും സ്വയം ചേർക്കനും/നീക്കം ചെയ്യനും കഴിയും");
+define("UCSLAN_33", "");
+define("UCSLAN_36", "");
+define("UCSLAN_37", "");
+define("UCSLAN_38", "");
+define("UCSLAN_39", "");
+define("UCSLAN_40", "");
+define("UCSLAN_41", "");
+define("UCSLAN_43", "");
+define("UCSLAN_45", "");
+define("UCSLAN_46", "");
+define("UCSLAN_47", "");
+define("UCSLAN_48", "");
+define("UCSLAN_49", "");
+define("UCSLAN_53", "");
+define("UCSLAN_54", "");
+define("UCSLAN_55", "");
+define("UCSLAN_56", "");
+define("UCSLAN_57", "");
+define("UCSLAN_58", "");
+define("UCSLAN_62", "");
+define("UCSLAN_63", "");
+define("UCSLAN_64", "");
+define("UCSLAN_65", "");
+define("UCSLAN_69", "");
+define("UCSLAN_70", "");
+define("UCSLAN_71", "");
+define("UCSLAN_72", "");
+define("UCSLAN_73", "");
+define("UCSLAN_74", "");
+define("UCSLAN_75", "");
+define("UCSLAN_76", "");
+define("UCSLAN_77", "");
+define("UCSLAN_78", "");
+define("UCSLAN_80", "");
+define("UCSLAN_81", "");
+define("UCSLAN_82", "");
+define("UCSLAN_83", "");
+define("UCSLAN_85", "");
+define("UCSLAN_86", "ചില ക്രമീകരണങ്ങൾ അഡ്മിൻ ക്ലാസുകൾക്ക് അനുവദനീയമല്ല - അവ ഡീഫോൾട് ആയി സജ്ജമാക്കിയിരിക്കുന്നു.");
+define("UCSLAN_87", "അടുത്തിടെ ചേർന്ന ഉപയോക്താക്കൾ");
+define("UCSLAN_88", "തിരിച്ചറിഞ്ഞ തിരയൽ ബോട്ടുകൾ");
+define("UCSLAN_89", "ചെക്കുചെയ്ത ക്ലാസുകൾ ഗ്രൂപ്പിലെ അംഗങ്ങളാണ്");
+define("UCSLAN_90", "നിങ്ങൾക്ക് ചില സിസ്റ്റം യൂസർ ക്ലാസുകൾ എഡിറ്റുചെയ്യാൻ കഴിയില്ല!");
+define("UCSLAN_91", "ക്ലാസ് ഘടന");
+
+
+?>
