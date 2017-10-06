784a4e11192cf7447034c0f562e88e81ecec2297
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-07 00:12:12 +0400

    Add eurl terms for admin - complete

---
+++ e107_languages/Malayalam/admin/lan_eurl.php
@@ -0,0 +1,96 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/06 18:00:16
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("LAN_EURL_NAME", "സൈറ്റ് URL കൾ മാനേജ് ചെയ്യുക");
+define("LAN_EURL_NAME_CONFIG", "പ്രൊഫൈലുകൾ");
+define("LAN_EURL_NAME_ALIASES", "അപരനാമങ്ങൾ");
+define("LAN_EURL_NAME_SETTINGS", "പൊതുവായ ക്രമീകരണങ്ങൾ");
+define("LAN_EURL_NAME_HELP", "സഹായം");
+define("LAN_EURL_EMPTY", "ലിസ്റ്റ് ശൂന്യമാണ്");
+define("LAN_EURL_LEGEND_CONFIG", "സൈറ്റ് ഏരിയയുടെ URL പ്രൊഫൈൽ തിരഞ്ഞെടുക്കുക");
+define("LAN_EURL_LEGEND_ALIASES", "അടിസ്ഥാന URL അപരനാമങ്ങൾ ഓരോ URL പ്രൊഫൈലിനും ക്രമീകരിക്കുക");
+define("LAN_EURL_DEFAULT", "ഡീഫോൾട്");
+define("LAN_EURL_PROFILE", "പ്രൊഫൈൽ");
+define("LAN_EURL_INFOALT", "വിവരം");
+define("LAN_EURL_PROFILE_INFO", "പ്രൊഫൈൽ വിവരം ലഭ്യമല്ല");
+define("LAN_EURL_LOCATION", "പ്രൊഫൈൽ സ്ഥാനം");
+define("LAN_EURL_LOCATION_NONE", "കോൺഫിഗറേഷൻ ഫയൽ ലഭ്യമല്ല");
+define("LAN_EURL_FORM_HELP_DEFAULT", "ഡീഫോൾട് ഭാഷയിൽ ആയിരിക്കുമ്പോൾ ഉള്ള അപരനാമം");
+define("LAN_EURL_FORM_HELP_ALIAS_0", "ഡീഫോൾട് മൂല്യം ആണ്");
+define("LAN_EURL_FORM_HELP_ALIAS_1", "ഇതിൽ ആകുമ്പോൾ ഉള്ള അപരനാമം");
+define("LAN_EURL_FORM_HELP_EXAMPLE", "ബേസ് URL");
+define("LAN_EURL_ERR_ALIAS_MODULE", "അപരനാമം \"%1\$s\" സംരക്ഷിക്കാൻ കഴിയില്ല - സമാന നാമമുള്ള ഒരു സിസ്റ്റം URL പ്രൊഫൈൽ ഉണ്ട്. സിസ്റ്റം URL പ്രൊഫൈൽ \"%2\$s\" എന്നതിനായി മറ്റൊരു അപരനാമ മൂല്യം ദയവായി തിരഞ്ഞെടുക്കുക.");
+define("LAN_EURL_SURL_UPD", "  SEF URLകൾ അപ്ഡേറ്റുചെയ്തു.");
+define("LAN_EURL_SURL_NUPD", "  SEF URLകൾ അപ്ഡേറ്റുചെയ്തില്ല.");
+define("LAN_EURL_SETTINGS_PATHINFO", "URLൽ നിന്ന് ഫയൽനാമം നീക്കം ചെയ്യുക");
+define("LAN_EURL_SETTINGS_MAINMODULE", "റൂട്ട് നെയിംസ്പേസ് അസോസിയേറ്റ് ചെയ്യുക");
+define("LAN_EURL_SETTINGS_MAINMODULE_HELP", "നിങ്ങളുടെ സൈറ്റ് സൈറ്റ് URL- ൽ ഏത് സൈറ്റ് ഏരിയയുമായി ബന്ധപ്പെടുമെന്നത് തിരഞ്ഞെടുക്കുക. ഉദാഹരണം: വാർത്ത നിങ്ങളുടെ റൂട്ട് നെയിംസ്പെയ്സ് ആയിരിക്കുമ്പോൾ: http://yoursite.com/News-Item-Title വാർത്തയുമായി ബന്ധപ്പെട്ടിരിക്കുന്നു (ഇനം കാഴ്ച പേജ് പരിഹരിക്കപ്പെടും)");
+define("LAN_EURL_SETTINGS_REDIRECT", "സിസ്റ്റം - പേജ് കണ്ടെത്താനായില്ല പേജിലേക്ക് റീഡയറക്ട് ചെയ്യുക");
+define("LAN_EURL_SETTINGS_REDIRECT_HELP", "ഫാൾസ് ആയി സജ്ജീകരിച്ചിട്ടുണ്ടെങ്കിൽ, പേജ് കണ്ടെത്താനായില്ല പേജ് നേരിട്ടുള്ള റെൻഡർ ആയിരിക്കും (ബ്രൌസർ റീഡയറക്ട് കൂടാതെ)");
+define("LAN_EURL_SETTINGS_SEFTRANSLATE", "ഓട്ടോമേറ്റഡ് SEF സ്ട്രിംഗ് ക്രിയേഷൻ തരം");
+define("LAN_EURL_SETTINGS_SEFTRANSLATE_HELP", "ഒരു ശീർഷക മൂല്യത്തിൽ നിന്ന് സ്വയം നിർമ്മിക്കുന്ന സമയത്ത് (ഉദാ: വാർത്തകൾ, ഇഷ്ടാനുസൃത പേജുകൾ മുതലായവയിൽ) എങ്ങനെയാണ് SEF സ്ട്രിംഗ് രൂപീകരിക്കേണ്ടതെന്ന് തിരഞ്ഞെടുക്കുക");
+define("LAN_EURL_SETTINGS_SEFTRTYPE_NONE", "സംരക്ഷിക്കുക മാത്രം ചെയ്യുക");
+define("LAN_EURL_SETTINGS_SEFTRTYPE_DASHL", "ലോവർ-കേസിലേക്ക്-ഡാഷെറൈസ്-ചെയ്യുക");
+define("LAN_EURL_SETTINGS_SEFTRTYPE_DASHC", "കാമൽ-കേസിലേക്ക്-ഡാഷെറൈസ്-ചെയ്യുക");
+define("LAN_EURL_SETTINGS_SEFTRTYPE_DASH", "കേസ്-മാറ്റമില്ലാതെ-ഡാഷെറൈസ്-ചെയ്യുക");
+define("LAN_EURL_SETTINGS_SEFTRTYPE_UNDERSCOREL", "ലോവർ_കേസിലേക്ക്_അണ്ടർസ്കോർ_ചെയ്യുക");
+define("LAN_EURL_SETTINGS_SEFTRTYPE_UNDERSCOREC", "കാമൽ_കേസിലേക്ക്_അണ്ടർസ്കോർ_ചെയ്യുക");
+define("LAN_EURL_SETTINGS_SEFTRTYPE_UNDERSCORE", "മാറ്റം_ഒന്നും_ഇല്ലാതെ_അണ്ടർസ്കോർ_ചെയ്യുക");
+define("LAN_EURL_SETTINGS_SEFTRTYPE_PLUSL", "ലോവർ+കേസിലേക്ക്+പ്ലസ്+സെപ്പറേറ്റർ+ഉപയോഗിക്കുക");
+define("LAN_EURL_SETTINGS_SEFTRTYPE_PLUSC", "കാമൽ+കേസിലേക്ക്+പ്ലസ്+സെപ്പറേറ്റർ+ഉപയോഗിക്കുക");
+define("LAN_EURL_SETTINGS_SEFTRTYPE_PLUS", "മാറ്റം+ഒന്നും+ഇല്ലാതെ+പ്ലസ്+സെപ്പറേറ്റർ+ഉപയോഗിക്കുക");
+define("LAN_EURL_MODREWR_DESCR", "നിങ്ങളുടെ URL കളിൽ നിന്നും എൻട്രി സ്ക്രിപ്റ്റ് ഫയൽ നാമം (index.php/) നീക്കം ചെയ്യുന്നു. നിങ്ങളുടെ സെർവറിൽ mod_rewrite ഇൻസ്റ്റാൾ ചെയ്യുകയും നിങ്ങളുടെ പ്രവർത്തിപ്പിക്കുകയും ചെയ്യേണ്ടതുണ്ട് (Apache Web Server). ഈ സജ്ജീകരണം പ്രാപ്തമാക്കിയതിന് ശേഷം, നിങ്ങളുടെ സൈറ്റ് റൂട്ട് ഫോൾഡറിലേക്ക് പോയി, htaccess.txt എന്നത് .htaccess എന്നാക്കുക, അതിനുശേഷം <em> \"RewriteBase\" </em> വേണമെങ്കിൽ പരിഷ്‌കരിക്കുക.");
+define("LAN_EURL_MENU", "സൈറ്റ് URL കൾ");
+define("LAN_EURL_MENU_CONFIG", "കോൺഫിഗറേഷൻ");
+define("LAN_EURL_MENU_ALIASES", "പ്രൊഫൈൽ അപരനാമങ്ങൾ");
+define("LAN_EURL_MENU_SETTINGS", "ക്രമീകരണങ്ങൾ");
+define("LAN_EURL_MENU_HELP", "സഹായം");
+define("LAN_EURL_MENU_PROFILES", "പ്രൊഫൈലുകൾ");
+define("LAN_EURL_UC", "പണിപ്പുരയിൽ");
+define("LAN_EURL_CORE_MAIN", "സൈറ്റ് റൂട്ട് നെയിംസ്പേസ് - അപരനാമം ഉപയോഗത്തിലില്ല.");
+define("LAN_EURL_FRIENDLY", "സൗഹൃദ");
+define("LAN_EURL_LEGACY", "ലെഗസി നേരിട്ടുള്ള URL കൾ.");
+define("LAN_EURL_REWRITE_LABEL", "സൗഹൃദ URL കൾ");
+define("LAN_EURL_REWRITE_DESCR", "തിരയൽ എഞ്ചിനും ഉപയോക്തൃ സൗഹാർദ്ദപരവുമായ URL കൾ.");
+define("LAN_EURL_CORE_NEWS", "ന്യൂസ്");
+define("LAN_EURL_NEWS_REWRITEF_LABEL", "പൂർണ്ണ സൌഹൃദപരമായ URL കൾ (ഏറ്റവും സൗഹൃദപരം, നിര്‍വ്വഹണപരത ഒട്ടുമില്ല)");
+define("LAN_EURL_NEWS_REWRITEF_DESCR", "");
+define("LAN_EURL_NEWS_REWRITE_LABEL", "ID ഇല്ലാത്ത സൗഹൃദ URL-കൾ (കൂടുതൽ സൗഹൃദപരം, നിര്‍വ്വഹണപരത ഒട്ടുമില്ല)");
+define("LAN_EURL_NEWS_REWRITE_DESCR", "മാനുവൽ ലിങ്ക് പാർസിങ്ങും, അസ്സെംബ്ലിങ്ങും പ്രദർശിപ്പിക്കുന്നു.");
+define("LAN_EURL_NEWS_REWRITEX_LABEL", "ഐഡി ഉള്ള സൗഹൃദപരമായ URL കൾ (നിര്‍വ്വഹണപരത പ്രകാരം) ");
+define("LAN_EURL_NEWS_REWRITEX_DESCR", "മുൻനിർവ്വചിത റൂട്ട് ചട്ടങ്ങൾ അടിസ്ഥാനമാക്കി ഓട്ടോമാറ്റിക് ലിങ്ക് പാഴ്സുചെയ്യുന്നതിനും അസ്സെംബ്ലിങ്ങും ഇത് പ്രദർശിപ്പിക്കുന്നു.");
+define("LAN_EURL_CORE_USER", "ഉപയോക്താക്കൾ");
+define("LAN_EURL_USER_REWRITE_LABEL", "സൗഹാർദ്ദ URL കൾ");
+define("LAN_EURL_USER_REWRITE_DESCR", "തിരയൽ എഞ്ചിൻ ഉപയോക്തൃ സൗഹൃദ URL കൾ.");
+define("LAN_EURL_CORE_PAGE", "കസ്റ്റം പേജുകൾ");
+define("LAN_EURL_PAGE_SEF_LABEL", "ID ഉള്ള സൌഹൃദപരമായ URL കൾ (നിര്‍വ്വഹണപരത) ");
+define("LAN_EURL_PAGE_SEF_DESCR", "തിരയൽ എഞ്ചിനും ഉപയോക്തൃ സൗഹാർദ്ദപരവുമായ URL കൾ.");
+define("LAN_EURL_PAGE_SEFNOID_LABEL", "ID ഇല്ലാത്ത സൗഹൃദ URL - കൾ (നിര്‍വ്വഹണപരത ഇല്ല, കൂടുതൽ സൗഹൃദപരം)");
+define("LAN_EURL_PAGE_SEFNOID_DESCR", "തിരയൽ എഞ്ചിനും ഉപയോക്തൃ സൗഹാർദ്ദപരവുമായ URL കൾ.");
+define("LAN_EURL_CORE_SEARCH", "തിരയൽ");
+define("LAN_EURL_SEARCH_DEFAULT_LABEL", "ഡീഫോൾട് തിരയൽ URL");
+define("LAN_EURL_SEARCH_DEFAULT_DESCR", "ലെഗസി നേരിട്ടുള്ള URL.");
+define("LAN_EURL_SEARCH_REWRITE_LABEL", "സൗഹൃദ URL");
+define("LAN_EURL_SEARCH_REWRITE_DESCR", "");
+define("LAN_EURL_CORE_SYSTEM", "സിസ്റ്റം");
+define("LAN_EURL_SYSTEM_DEFAULT_LABEL", "ഡീഫോൾട് സിസ്റ്റം URL-കൾ");
+define("LAN_EURL_SYSTEM_DEFAULT_DESCR", "പേജ് കണ്ടെത്താനായില്ല,  ആക്സസ് നിരസിച്ചു എന്നിവപോലുള്ള പേജുകൾക്കുള്ള URL കൾ.");
+define("LAN_EURL_SYSTEM_REWRITE_LABEL", "സൗഹൃദ സിസ്റ്റം URLകൾ");
+define("LAN_EURL_SYSTEM_REWRITE_DESCR", "പേജ് കണ്ടെത്താനായില്ല,  ആക്സസ് നിരസിച്ചു എന്നിവപോലുള്ള പേജുകൾക്കുള്ള URL കൾ.");
+define("LAN_EURL_CORE_INDEX", "ഫ്രന്റ് പേജ്");
+define("LAN_EURL_CORE_INDEX_INFO", "ഫ്രന്റ് പേജിനു അപരനാമമുണ്ടാകാൻ പാടില്ല.");
+define("LAN_EURL_REBUILD", "പുനർനിർമ്മിക്കുക");
+define("LAN_EURL_REGULAR_EXPRESSION", "റെഗുലർ എക്സ്പ്രഷൻ");
+define("LAN_EURL_KEY", "കീ");
+define("LAN_EURL_TABLE", "പട്ടിക");
+
+
+?>
