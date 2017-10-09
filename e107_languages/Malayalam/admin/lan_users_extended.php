aafcb02d07a7eef70710bfb381f319ab721ff104
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-09 19:50:43 +0400

    Add admin terms for users_extended - partial

---
+++ e107_languages/Malayalam/admin/lan_users_extended.php
@@ -0,0 +1,108 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/09 15:11:16
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("EXTLAN_1", "പേര്");
+define("EXTLAN_2", "പ്രിവ്യൂ ചെയ്യുക");
+define("EXTLAN_3", "മൂല്യങ്ങൾ");
+define("EXTLAN_4", "ആവശ്യം");
+define("EXTLAN_5", "ബാധകമായത്");
+define("EXTLAN_6", "റീഡ് അക്സസ്സ്");
+define("EXTLAN_7", "റൈറ്റ് ആക്സസ്");
+define("EXTLAN_8", "പ്രവർത്തനം");
+define("EXTLAN_9", "വിപുലീകരിച്ച ഉപയോക്തൃ ഫീൾഡുകൾ");
+define("EXTLAN_10", "ഫീൽഡ് പേര്");
+define("EXTLAN_11", "");
+define("EXTLAN_12", "ഫീൽഡ് ടെക്സ്റ്റ്");
+define("EXTLAN_13", "");
+define("EXTLAN_14", "ഫീൽഡ് തരം");
+define("EXTLAN_15", "ഫീൽഡിൽ ഉൾപ്പെടുന്ന ടെക്സ്റ്റ്");
+define("EXTLAN_16", "ഡീഫോൾട് മൂല്യം");
+define("EXTLAN_17", "");
+define("EXTLAN_18", "ആവശ്യമാണ്");
+define("EXTLAN_19", "");
+define("EXTLAN_20", "");
+define("EXTLAN_21", "");
+define("EXTLAN_22", "");
+define("EXTLAN_23", "");
+define("EXTLAN_24", "");
+define("EXTLAN_25", "താഴേക്ക് നീക്കുക");
+define("EXTLAN_26", "മുകളിലേക്ക് നീക്കുക");
+define("EXTLAN_27", "ഇല്ലാതാക്കൽ സ്ഥിരീകരിക്കുക");
+define("EXTLAN_28", "");
+define("EXTLAN_29", "");
+define("EXTLAN_30", "");
+define("EXTLAN_31", "");
+define("EXTLAN_32", "");
+define("EXTLAN_33", "");
+define("EXTLAN_34", "");
+define("EXTLAN_35", "");
+define("EXTLAN_36", "");
+define("EXTLAN_37", "");
+define("EXTLAN_38", "");
+define("EXTLAN_39", "");
+define("EXTLAN_40", "");
+define("EXTLAN_41", "");
+define("EXTLAN_42", "");
+define("EXTLAN_43", "");
+define("EXTLAN_44", "");
+define("EXTLAN_45", "");
+define("EXTLAN_46", "");
+define("EXTLAN_47", "");
+define("EXTLAN_48", "");
+define("EXTLAN_49", "");
+define("EXTLAN_50", "");
+define("EXTLAN_51", "");
+define("EXTLAN_52", "");
+define("EXTLAN_53", "");
+define("EXTLAN_54", "");
+define("EXTLAN_55", "");
+define("EXTLAN_56", "");
+define("EXTLAN_57", "സജീവമാക്കി");
+define("EXTLAN_58", "സജീവമാക്കിയില്ല");
+define("EXTLAN_59", "സജീവമാക്കുക");
+define("EXTLAN_60", "നിർജ്ജീവമാക്കുക");
+define("EXTLAN_62", "പട്ടിക");
+define("EXTLAN_63", "ഫീൽഡ് ഐഡി");
+define("EXTLAN_64", "പ്രദർശന മൂല്യം");
+define("EXTLAN_65", "");
+define("EXTLAN_66", "");
+define("EXTLAN_67", "");
+define("EXTLAN_68", "");
+define("EXTLAN_69", "");
+define("EXTLAN_70", "");
+define("EXTLAN_71", "");
+define("EXTLAN_72", "");
+define("EXTLAN_73", "");
+define("EXTLAN_74", "");
+define("EXTLAN_75", "");
+define("EXTLAN_76", "");
+define("EXTLAN_77", "");
+define("EXTLAN_78", "");
+define("EXTLAN_79", "");
+define("EXTLAN_80", "");
+define("EXTLAN_81", "");
+define("EXTLAN_82", "");
+define("EXTLAN_83", "");
+define("EXTLAN_84", "");
+define("EXTLAN_86", "");
+define("EXTLAN_87", "");
+define("EXTLAN_HELP_1", "");
+define("EXTLAN_HELP_2", "");
+define("EXTLAN_HELP_3", "");
+define("EXTLAN_HELP_4", "");
+define("EXTLAN_HELP_5", "");
+define("EXTLAN_HELP_6", "");
+define("EXTLAN_HELP_7", "");
+define("EXTLAN_HELP_8", "");
+define("EXTLAN_HELP_9", "");
+
+
+?>
