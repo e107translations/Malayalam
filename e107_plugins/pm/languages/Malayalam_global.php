c03458b0bf10f9f92bbd7a08d3f2ba6a31edb99d
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-13 16:16:35 +0400

    Add PM global terms - full

---
+++ e107_plugins/pm/languages/Malayalam_global.php
@@ -0,0 +1,30 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/13 12:15:36
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_PM_NAME", "പ്രൈവറ്റ് മെസഞ്ചർ");
+define("LAN_PLUGIN_PM_DESCRIPTION", "ഈ പ്ലഗിൻ ഒരു പൂർണ്ണമായ സ്വകാര്യ മെസ്സേജിംഗ് സംവിധാനമാണ്.");
+define("LAN_PLUGIN_PM_URL_DEFAULT_LABEL", "ഡീഫോൾട്");
+define("LAN_PLUGIN_PM_URL_DEFAULT_DESCR", "ഡീഫോൾട്");
+define("LAN_PLUGIN_PM_INBOX", "ഇൻബോക്സ്");
+define("LAN_PLUGIN_PM_OUTBOX", "ഔട്ട്ബോക്സ്");
+define("LAN_PLUGIN_PM_NEW", "പുതിയ സന്ദേശം അയയ്ക്കുക");
+define("LAN_PLUGIN_PM_TO", "ലേക്ക്");
+define("LAN_PLUGIN_PM_FROM", "മുതൽ");
+define("LAN_PLUGIN_PM_SUB", "വിഷയം");
+define("LAN_PLUGIN_PM_MESS", "സന്ദേശം");
+define("LAN_PLUGIN_PM_READ", "വായിക്കുക");
+define("LAN_PLUGIN_PM_DEL", "PM ഇല്ലാതാക്കുക");
+define("LAN_PLUGIN_PM_ATTACHMENT", "ബന്ധം");
+define("LAN_PLUGIN_PM_SIZE", "വലുപ്പം");
+
+
+?>
