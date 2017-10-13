4ac81b6618f3c7a3983d1a564599b2991bdd0b9a
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-14 01:32:22 +0400

    Add news plugin terms - complete :100:

---
+++ e107_plugins/news/languages/Malayalam.php
@@ -0,0 +1,19 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/27 23:36:30
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("TD_MENU_L1", "ന്യൂസ്വാർത്തകൾ");
+define("TD_MENU_L2", "മറ്റ് ന്യൂസ്");
+define("LAN_NEWSCAT_MENU_TITLE", "ന്യൂസ് വിഭാഗങ്ങൾ");
+define("LAN_NEWSLATEST_MENU_TITLE", "ഏറ്റവും പുതിയ ന്യൂസ്");
+define("LAN_NEWSARCHIVE_MENU_TITLE", "ന്യൂസ് ആർക്കൈവ്");
+
+
+?>
---
+++ e107_plugins/news/languages/Malayalam_admin.php
@@ -0,0 +1,27 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/27 23:40:26
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_NEWS_ADMIN_00", "പുതിയ വാർത്താ ഇനങ്ങൾ");
+define("LAN_NEWS_ADMIN_01", "സ്റ്റിക്കി വാർത്താ ഇനങ്ങൾ");
+define("LAN_NEWS_ADMIN_02", "അസൈൻഡ് ന്യൂസ് ഇനങ്ങൾ");
+define("LAN_NEWS_ADMIN_03", "ഒരു പ്രത്യേക വിഭാഗത്തിലേക്ക് വാർത്താ ഇനങ്ങളെ പരിമിതപ്പെടുത്തുക");
+define("LAN_NEWS_ADMIN_04", "'വാർത്താ ഗ്രിഡ് മെനു' എന്നതിന് നൽകിയിരിക്കുന്ന ഒരു ടെംപ്ലേറ്റാണ് അസൈൻ ചെയ്ത ഇനങ്ങൾ");
+define("LAN_NEWS_ADMIN_05", "പ്രദർശിപ്പിക്കേണ്ട ഇനങ്ങളുടെ എണ്ണം");
+define("LAN_NEWS_ADMIN_06", "ശീർഷക അക്ഷരപ്രതീക പരിധി");
+define("LAN_NEWS_ADMIN_07", "സംഗ്രഹ അക്ഷരപ്രതീക പരിധി");
+define("LAN_NEWS_ADMIN_08", "ആർക്കൈവ് ലിങ്ക് പ്രദർശിപ്പിക്കുക");
+define("LAN_NEWS_ADMIN_09", "പരിധികൾ");
+define("LAN_NEWS_ADMIN_10", "ഫീച്ചർ ഇനങ്ങൾ എണ്ണം");
+define("LAN_NEWS_ADMIN_11", "'ന്യൂസ് കരോസൽ' നിർണ്ണയിച്ചിട്ടുള്ള ഒരു ടെംപ്ലേറ്റാണ് അസൈൻ ചെയ്ത ഇനങ്ങൾ");
+
+
+?>
