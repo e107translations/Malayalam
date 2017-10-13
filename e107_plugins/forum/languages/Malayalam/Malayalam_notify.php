426449c5a468241f6f2b8a5f1d339db12cba3d4f
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-13 23:48:39 +0400

    Add forum plugin notify and search

---
+++ e107_plugins/forum/languages/Malayalam/Malayalam_notify.php
@@ -0,0 +1,26 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/13 19:45:04
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_FORUM_NT_7", "ഫോറം - പുതിയ ത്രെഡ് ഉപയോക്താവ് സൃഷ്ടിച്ചു");
+define("LAN_FORUM_NT_8", "ഫോറം - ത്രെഡ് ഇല്ലാതാക്കി");
+define("LAN_FORUM_NT_9", "ഫോറം - ത്രെഡ് സ്പ്ലിറ്റ് ചെയ്തു");
+define("LAN_FORUM_NT_10", "ഫോറം - പോസ്റ്റ് ഇല്ലാതാക്കി");
+define("LAN_FORUM_NT_11", "ഫോറം - പോസ്റ്റ് റിപ്പോർട്ടുചെയ്തു");
+define("LAN_FORUM_NT_NEWTOPIC", "പുതിയ വിഷയം സൃഷ്ടിച്ചു");
+define("LAN_FORUM_NT_NEWTOPIC_PROB", "പ്രൊബേഷണറി അംഗം പുതിയ വിഷയം സൃഷ്ടിച്ചു");
+define("LAN_FORUM_NT_TOPIC_DELETED", "വിഷയം ഇല്ലാതാക്കി");
+define("LAN_FORUM_NT_TOPIC_SPLIT", "വിഷയം സ്പ്ളിറ് ചെയ്തു");
+define("LAN_FORUM_NT_POST_DELETED", "പോസ്റ്റ് ഇല്ലാതാക്കി");
+define("LAN_FORUM_NT_POST_REPORTED", "പോസ്റ്റ് റിപ്പോർട്ടുചെയ്തു");
+
+
+?>
---
+++ e107_plugins/forum/languages/Malayalam/Malayalam_search.php
@@ -0,0 +1,18 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 15:13:40
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("FOR_SCH_LAN_2", "ഫോറം തിരഞ്ഞെടുക്കുക");
+define("FOR_SCH_LAN_4", "മുഴുവൻ പോസ്റ്റും");
+define("FOR_SCH_LAN_5", "ത്രെഡിന്റെ ഭാഗമായി");
+
+
+?>
