8f99a5cbb6463fd9ad90c6d29c442c58966079c9
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-14 00:02:13 +0400

    Add linkwords plugin log and global

---
+++ e107_plugins/linkwords/languages/Malayalam_global.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/13 19:59:03
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_LINKWORDS_NAME", "ലിങ്ക്-വാക്കുകൾ");
+define("LAN_PLUGIN_LINKWORDS_DESCRIPTION", "ഈ പ്ലഗിൻ നിർദ്ദിഷ്ട വാക്കുകൾ നിർവ്വചിക്കപ്പെട്ട ലിങ്കുമായോ/അല്ലെങ്കിൽ ടൂൾടിപ്പുമായോ ബന്ധിപ്പിക്കും.");
+
+
+?>
---
+++ e107_plugins/linkwords/languages/Malayalam_log.php
@@ -0,0 +1,21 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/13 20:00:23
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_AL_LINKWD_00", "ലിങ്ക്വേഡ്-അനുബന്ധ സന്ദേശം");
+define("LAN_AL_LINKWD_01", "ലിങ്ക്വേഡ് ചേർത്തു");
+define("LAN_AL_LINKWD_02", "ലിങ്ക്വേഡ് എഡിറ്റുചെയ്തു");
+define("LAN_AL_LINKWD_03", "ലിങ്ക്വേഡ് ഇല്ലാതാക്കി");
+define("LAN_AL_LINKWD_04", "ലിങ്ക്വേഡ് ഓപ്ഷനുകൾ അപ്ഡേറ്റുചെയ്തു");
+define("LAN_AL_LINKWD_05", "ലിങ്ക്വേഡ് പതിപ്പ് അപ്ഡേറ്റ് ചെയ്തു");
+
+
+?>
