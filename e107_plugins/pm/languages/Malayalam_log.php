126b0d6cefa446ebc8676826bd9d56e072a85391
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-13 16:13:33 +0400

    Add PM log terms - full

---
+++ e107_plugins/pm/languages/Malayalam_log.php
@@ -0,0 +1,25 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/13 12:10:45
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_AL_PM_ADM_01", "PM: ഡീഫോൾട് പ്രീഫെറെൻസ് ഉപയോഗിച്ചിരിക്കുന്നു");
+define("LAN_AL_PM_ADM_02", "PM: പ്രീഫെറെൻസ് അപ്ഡേറ്റ് ചെയ്തു");
+define("LAN_AL_PM_ADM_03", "PM: DB മെയിന്റനൻസ് പൂർത്തിയായി");
+define("LAN_AL_PM_ADM_04", "PM: DB മെയിൻറനൻസ് തുടങ്ങി");
+define("LAN_AL_PM_ADM_05", "PM: പരിധി ചേർത്തു");
+define("LAN_AL_PM_ADM_06", "PM: പരിധി അപ്ഡേറ്റ് ചെയ്തു");
+define("LAN_AL_PM_ADM_07", "PM: പരിധി ഇല്ലാതാക്കി");
+define("LAN_AL_PM_ADM_08", "PM: പരിധി ഡാറ്റ സൃഷ്ടിക്കുന്നതിൽ പിഴവ്");
+define("LAN_AL_PM_ADM_09", "PM: പരിധി ഡാറ്റ അപ്ഡേറ്റുചെയ്യുന്നതിൽ പിശക്");
+define("LAN_AL_PM_ADM_10", "PM: പരിധി ഡാറ്റ ഇല്ലാതാക്കുന്നതിൽ പിശക്");
+
+
+?>
