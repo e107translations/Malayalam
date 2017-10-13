01215909ebcd78de528d3ee87f1ff9bffbde00c3
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-13 23:55:06 +0400

    Add gallery plugin front and global

---
+++ e107_plugins/gallery/languages/Malayalam/Malayalam_front.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 15:43:08
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_GALLERY_FRONT_01", "വലത്-ക്ലിക്ക്> ലിങ്ക് സംരക്ഷിക്കുക");
+define("LAN_GALLERY_FRONT_02", "ചിത്രം വികസിപ്പിക്കുക");
+
+
+?>
---
+++ e107_plugins/gallery/languages/Malayalam/Malayalam_global.php
@@ -0,0 +1,21 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/13 19:53:04
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_GALLERY_TITLE", "ഗാലറി");
+define("LAN_PLUGIN_GALLERY_DIZ", "ഒരു ലളിതമായ ഇമേജ് ഗാലറി");
+define("LAN_PLUGIN_GALLERY_SEF_01", "ഗ്യാലറി SEF");
+define("LAN_PLUGIN_GALLERY_SEF_02", "SEF URL കൾ പ്രാപ്തമാക്കി.");
+define("LAN_PLUGIN_GALLERY_SEF_03", "SEF URL കൾ അപ്രാപ്തമാക്കി.");
+define("LAN_PLUGIN_GALLERY_SEF_04", "ഗാലറി ഡീഫോൾട്");
+
+
+?>
