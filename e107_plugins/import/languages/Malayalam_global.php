8e21979580c69ca1273ad3dcf90a83d33e3381f5
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-15 06:58:59 +0400

    Add import plugin terms - complete :100:

---
+++ e107_plugins/import/languages/Malayalam_global.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/13 19:56:27
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_IMPORT_NAME", "E107 ലേക്ക് ഇമ്പോർട്ട് ചെയ്യുക");
+define("LAN_PLUGIN_IMPORT_DESCRIPTION", "Wordpress, Joomla, Drupal, Blogpost, RSS മറ്റ് ഫോർമാറ്റുകളിൽ നിന്നുള്ള ഡാറ്റ ഇമ്പോർട്ട് ചെയ്യുക.");
+
+
+?>
