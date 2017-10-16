be20740a4a157634af0b97a857dab6bb526b74f0
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-16 14:15:48 +0400

    Add download plugin terms - except admin

---
+++ e107_plugins/download/languages/Malayalam/Malayalam_global.php
@@ -0,0 +1,16 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 15:41:25
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("LAN_PLUGIN_DOWNLOAD_NAME", "ഡൗൺലോഡുകൾ");
+define("LAN_PLUGIN_DOWNLOAD_DIZ", "ഈ പ്ലഗിൻ പൂർണ്ണ ഫീച്ചറുകളോടു കൂടിയ ഒരു ഫയൽ ഡൗൺലോഡ് സിസ്റ്റം ആണ്");
+
+
+?>
---
+++ e107_plugins/download/languages/Malayalam/Malayalam_log.php
@@ -0,0 +1,26 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/05 19:22:52
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_AL_DOWNL_01", "ഡൗൺലോഡുകൾ - ഡൗൺലോഡ് ഓപ്ഷനുകൾ മാറ്റി");
+define("LAN_AL_DOWNL_02", "ഡൗൺലോഡുകൾ - അപ്ലോഡ് ഓപ്ഷനുകൾ മാറ്റി");
+define("LAN_AL_DOWNL_03", "ഡൗൺലോഡുകൾ - പരിധി ചേർത്തു");
+define("LAN_AL_DOWNL_04", "ഡൗൺലോഡുകൾ - ലോഗ് 04");
+define("LAN_AL_DOWNL_05", "ഡൗൺലോഡുകൾ - ലോഗ് 05");
+define("LAN_AL_DOWNL_06", "ഡൗൺലോഡുകൾ - ലോഗ് 06");
+define("LAN_AL_DOWNL_07", "ഡൗൺലോഡുകൾ - ലോഗ് 07");
+define("LAN_AL_DOWNL_08", "ഡൗൺലോഡുകൾ - ലോഗ് 08");
+define("LAN_AL_DOWNL_09", "ഡൗൺലോഡുകൾ - ലോഗ് 09");
+define("LAN_AL_DOWNL_10", "ഡൗൺലോഡുകൾ - പരിധി അപ്ഡേറ്റ് ചെയ്യുക");
+define("LAN_AL_DOWNL_11", "ഡൗൺലോഡുകൾ - പരിധി നീക്കംചെയ്തു");
+
+
+?>
