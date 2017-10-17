2d97090bea390f8044d8dd3168edfa878be1a223
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-17 10:40:51 +0400

    Add list_new plugin terms - except admin

---
+++ e107_plugins/list_new/languages/Malayalam.php
@@ -0,0 +1,65 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/16 18:57:29
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("LIST_MENU_1", "പുതിയ കൂട്ടിച്ചേർക്കലുകൾ");
+define("LIST_MENU_2", "പ്രകാരം");
+define("LIST_MENU_3", "ല്‍");
+define("LIST_MENU_4", "അകത്ത്");
+define("LIST_MENU_5", "ദിവസങ്ങൾ");
+define("LIST_MENU_6", "എത്ര ദിവസത്തെ ഉള്ളടക്കം കാണണണം?");
+define("LIST_MENU_7", "");
+define("LIST_MENU_8", "");
+define("LIST_MENU_9", "");
+define("LIST_MENU_10", "");
+define("LIST_MENU_11", "");
+define("LIST_MENU_12", "");
+define("LIST_MENU_13", "");
+define("LIST_MENU_14", "");
+define("LIST_MENU_15", "");
+define("LIST_MENU_16", "");
+define("LIST_MENU_17", "");
+define("LIST_MENU_18", "");
+define("LIST_MENU_19", "");
+define("LIST_NEWS_1", "ന്യൂസ്");
+define("LIST_NEWS_2", "ന്യൂസ് ഇനങ്ങളൊന്നും ഇല്ല");
+define("LIST_COMMENT_1", "അഭിപ്രായങ്ങൾ");
+define("LIST_COMMENT_2", "അഭിപ്രായങ്ങൾ ഒന്നും ഇല്ല");
+define("LIST_COMMENT_3", "ന്യൂസ്");
+define("LIST_COMMENT_4", "എഫ്.എ.ക്യു");
+define("LIST_COMMENT_5", "പോൾ");
+define("LIST_COMMENT_6", "ഡോക്സ്");
+define("LIST_COMMENT_7", "ബഗ്ട്രാക്ക്");
+define("LIST_COMMENT_8", "കണ്ടെന്റ്");
+define("LIST_COMMENT_9", "");
+define("LIST_COMMENT_10", "ആശയങ്ങൾ");
+define("LIST_MEMBER_1", "അംഗങ്ങൾ");
+define("LIST_MEMBER_2", "അംഗങ്ങളില്ല");
+define("LIST_CONTENT_1", "ഉള്ളടക്കം");
+define("LIST_CONTENT_2", "ഉള്ളടക്കം ഇല്ല");
+define("LIST_CONTENT_3", "സാധുതയുള്ള ഉള്ളടക്ക വിഭാഗം ഇല്ല");
+define("LIST_CHATBOX_1", "ചാറ്റ്ബോക്സ്");
+define("LIST_CHATBOX_2", "ചാറ്റ്ബോക്സ് പോസ്റ്റുകൾ ഇല്ല");
+define("LIST_CALENDAR_1", "കലണ്ടർ");
+define("LIST_CALENDAR_2", "കലണ്ടർ ഇവന്റുകൾ ഇല്ല");
+define("LIST_LINKS_1", "ലിങ്കുകൾ");
+define("LIST_LINKS_2", "ലിങ്കുകളില്ല");
+define("LIST_FORUM_1", "ഫോറം");
+define("LIST_FORUM_2", "ഫോറം പോസ്റ്റുകളൊന്നുമില്ല");
+define("LIST_FORUM_3", "കാഴ്ചകൾ:");
+define("LIST_FORUM_4", "മറുപടികൾ:");
+define("LIST_FORUM_5", "അന്ത്യ പോസ്റ്റ്:");
+define("LIST_FORUM_6", "ൽ");
+define("LIST_LAN_1", "ഇനങ്ങളില്ല ഇതിൽ");
+define("LIST_DOWNLOAD_1", "ഡൌൺലോഡുകൾ");
+define("LIST_DOWNLOAD_2", "ഡൗൺലോഡുകളൊന്നുമില്ല");
+
+
+?>
