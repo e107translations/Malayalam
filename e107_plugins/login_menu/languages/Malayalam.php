58454517411656f85a60738ad3781e7839b2b41f
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-17 21:28:10 +0400

    Add login_menu terms - complete :100:

---
+++ e107_plugins/login_menu/languages/Malayalam.php
@@ -0,0 +1,56 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/26 10:14:03
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("LAN_LOGINMENU_1", "ഉപയോക്തൃനാമം:");
+define("LAN_LOGINMENU_2", "പാസ്വേഡ്:");
+define("LAN_LOGINMENU_3", "സൈൻ അപ്പ്");
+define("LAN_LOGINMENU_4", "പാസ്വേഡ് മറന്നോ?");
+define("LAN_LOGINMENU_5", "സ്വാഗതം");
+define("LAN_LOGINMENU_6", "എന്നെ ഓർക്കണം");
+define("LAN_LOGINMENU_7", "തനതായ ഉപയോക്തൃ ഐഡി തിരിച്ചറിയാൻ സാധിച്ചില്ല (സംഭവ്യത: കേടായ കുക്കി). കുക്കി നശിപ്പിക്കാൻ ദയവായി താഴെയുള്ള ലോഗ്ഔട്ട് ലിങ്ക് ക്ലിക്കുചെയ്യുക.");
+define("LAN_LOGINMENU_9", "ലോഗിൻ പിശക്");
+define("LAN_LOGINMENU_10", "അറ്റകുറ്റപ്പണി ഫ്ലാഗ് ഓൺ ആണ് - ഇതിനർത്ഥം  സാധാരണ സന്ദർശകർ sitedown.php - ലേക്ക് റീഡയറക്ട് ചെയ്യപ്പെടും എന്നതാണ്. ഫ്ലാഗ് ഓഫ് ചെയ്യുവാൻ  അഡ്മിൻ / മെയിൻറനൻസ് എന്ന മേഖലയിലേക്ക് പോകുക.");
+define("LAN_LOGINMENU_11", "അഡ്മിൻ മേഖല");
+define("LAN_LOGINMENU_13", "പ്രൊഫൈൽ");
+define("LAN_LOGINMENU_14", "വാർത്താ ഇനം");
+define("LAN_LOGINMENU_15", "വാർത്താ ഇനങ്ങൾ");
+define("LAN_LOGINMENU_16", "ചാറ്റ്ബോക്സ് പോസ്റ്റ്");
+define("LAN_LOGINMENU_17", "ചാറ്റ്ബോക്സ് പോസ്റ്റുകൾ");
+define("LAN_LOGINMENU_18", "അഭിപ്രായം");
+define("LAN_LOGINMENU_19", "അഭിപ്രായങ്ങൾ");
+define("LAN_LOGINMENU_20", "ഫോറം പോസ്റ്റ്");
+define("LAN_LOGINMENU_21", "ഫോറം പോസ്റ്റുകൾ");
+define("LAN_LOGINMENU_22", "പുതിയ സൈറ്റ് അംഗം");
+define("LAN_LOGINMENU_23", "പുതിയ സൈറ്റ് അംഗങ്ങൾ");
+define("LAN_LOGINMENU_24", "പുതിയ ഇനങ്ങളുടെ പട്ടിക കാണാൻ ഇവിടെ ക്ലിക്കുചെയ്യുക");
+define("LAN_LOGINMENU_25", "നിങ്ങളുടെ അവസാന സന്ദർശനത്തിനു ശേഷം ഉണ്ടായിട്ടുള്ളത്");
+define("LAN_LOGINMENU_26", "പൂജ്യം");
+define("LAN_LOGINMENU_27", "ഒപ്പം");
+define("LAN_LOGINMENU_31", "പുതിയ വാർത്ത പോസ്റ്റുകൾ കാണിക്കുക");
+define("LAN_LOGINMENU_34", "പുതിയ അഭിപ്രായ കുറിപ്പുകൾ കാണിക്കുക");
+define("LAN_LOGINMENU_36", "പുതിയ അംഗങ്ങളെ കാണിക്കുക");
+define("LAN_LOGINMENU_39", "അഡ്മിൻ വിടുക");
+define("LAN_LOGINMENU_40", "സജീവമാക്കൽ ഇമെയിൽ വീണ്ടും അയയ്ക്കുക");
+define("LAN_LOGINMENU_41", "ലോഗിൻ മെനു ക്രമീകരണങ്ങൾ");
+define("LAN_LOGINMENU_37", "കാണിക്കുക");
+define("LAN_LOGINMENU_38", "ലോഗിൻ മെനു - കൂടുതൽ ലിങ്കുകൾ");
+define("LAN_LOGINMENU_42", "ലോഗിൻ മെനു - അടുത്തിടെയുള്ള പ്രധാന കൂട്ടിച്ചേർക്കലുകൾ");
+define("LAN_LOGINMENU_43", "സ്ഥാനം");
+define("LAN_LOGINMENU_44", "നഷ്ടപ്പെട്ട ലിങ്ക് ശീർഷകം");
+define("LAN_LOGINMENU_45", "ലിങ്ക് (കൾ) -");
+define("LAN_LOGINMENU_45a", " ");
+define("LAN_LOGINMENU_45b", "പ്ലഗിൻ");
+define("LAN_LOGINMENU_46", "സമീപകാല ഇനങ്ങൾ -");
+define("LAN_LOGINMENU_47", "ലോഗിൻ മെനു - സമീപകാലത്തെ പ്ലഗിൻ കൂട്ടിച്ചേർക്കലുകൾ");
+define("LAN_LOGINMENU_48", "മെനു ക്രമീകരണം");
+define("LAN_LOGINMENU_49", "ഇമെയിൽ:");
+define("LAN_LOGINMENU_50", "ഉപയോക്തൃനാമം അല്ലെങ്കിൽ ഇമെയിൽ:");
+define("LAN_LOGINMENU_51", "സൈൻ ഇൻ");
