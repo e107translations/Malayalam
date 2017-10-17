13dade7a97e6b836c9e29343c92541a45ddca944
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-17 19:11:19 +0400

    Add online plugin terms - complete :100:

---
+++ e107_plugins/online/languages/Malayalam.php
@@ -0,0 +1,33 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/17 14:59:40
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("LAN_LASTSEEN_1", "അവസാനം കണ്ട മെനു");
+define("LAN_ONLINE_TRACKING_MESSAGE", "നിലവിൽ ഓൺലൈൻ ഉപയോക്തൃ ട്രാക്കിംഗ് അപ്രാപ്തമാക്കിയിരിക്കുന്നു, ദയവായി അത് [here] പ്രവർത്തനക്ഷമമാക്കുക");
+define("LAN_ONLINE_1", "അതിഥികൾ:");
+define("LAN_ONLINE_2", "അംഗങ്ങൾ:");
+define("LAN_ONLINE_3", "ഈ പേജിൽ:");
+define("LAN_ONLINE_4", "ഓൺലൈനിൽ");
+define("LAN_ONLINE_6", "ഏറ്റവും പുതിയ അംഗം:");
+define("LAN_ONLINE_7", "കാണുന്നു");
+define("LAN_ONLINE_8", "ഏറ്റവും കൂടുതൽ ഓൺലൈൻ ആയത്:");
+define("LAN_ONLINE_9", "ഇൽ");
+define("LAN_ONLINE_10", "ഓൺലൈൻ മെനു");
+define("LAN_ONLINE_11", "ആകെ രജിസ്റ്റർ ചെയ്ത അംഗങ്ങൾ:");
+define("LAN_ONLINE_ADMIN_1", "അവസാനം കണ്ട മെനു");
+define("LAN_ONLINE_ADMIN_2", "ക്യാപ്ഷൻ അവസാനം കണ്ട മെനു");
+define("LAN_ONLINE_ADMIN_3", "പ്രദർശിപ്പിക്കാനുള്ള റെക്കോർഡുകളുടെ എണ്ണം");
+define("LAN_ONLINE_ADMIN_4", "ഓൺലൈൻ മെനു");
+define("LAN_ONLINE_ADMIN_5", "ക്യാപ്ഷൻ ഓൺലൈൻ മെനു");
+define("LAN_ONLINE_ADMIN_6", "ഓൺലൈൻ അംഗങ്ങളുടെ പട്ടിക കാണിക്കണോ?");
+define("LAN_ONLINE_ADMIN_7", "ഓൺലൈൻ അംഗങ്ങളുടെ വിപുലീകൃത ലിസ്റ്റ് കാണിക്കണോ?");
+define("LAN_ONLINE_ADMIN_8", "അംഗങ്ങളുടെ ഒരു കോമയാൽ വേർതിരിച്ച ലിസ്റ്റ് പ്രദർശിപ്പിക്കുന്നു.");
+define("LAN_ONLINE_ADMIN_9", "ഒരു പേജ് കാണുന്ന അംഗങ്ങളുടെ പട്ടിക പ്രദർശിപ്പിക്കുന്നു.");
+define("LAN_ONLINE_ADMIN_10", "അതിഥികളെ ഓൺലൈനായി പ്രദർശിപ്പിക്കുക.");
