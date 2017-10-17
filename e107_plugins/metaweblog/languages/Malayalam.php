d24684f9b3d7cac2351488642cf802e6688b843f
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-17 14:54:09 +0400

    Add metaweblog terms - complete :100:

---
+++ e107_plugins/metaweblog/languages/Malayalam.php
@@ -0,0 +1,32 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/17 10:45:23
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("XMLRPC_ADMIN_001", "പ്രധാന മെനു");
+define("XMLRPC_CONFIG_001", "മെറ്റാവെബ്‌ലോഗ് :: കോൺഫിഗറേഷൻ");
+define("XMLRPC_PREFS_001", "eXMLRPC - ഓപ്ഷനുകൾ");
+define("XMLRPC_PREFS_002", "eXMLRPC");
+define("XMLRPC_PREFS_003", "ന്യൂസ് റെൻഡർ തരം (0,1,2,3)");
+define("XMLRPC_PREFS_004", "അപ്ലോഡ് ഡയറക്ടറി (e_FILE/)");
+define("XMLRPC_PREFS_005", "ക്ലയന്റിനായുള്ള ബ്ലോഗ് ഐഡി");
+define("XMLRPC_PREFS_006", "ക്ലയന്റിനായുള്ള ബ്ലോഗ് നാമം");
+define("XMLRPC_HELP_001", "നിർദ്ദേശങ്ങൾ");
+define("XMLRPC_HELP_010", "ജനറൽ");
+define("XMLRPC_HELP_011", "ഈ പ്ലഗിൻ തന്നെ ഒന്നും ചെയ്യുന്നില്ല! ഇത് ചില XMLRPC varibles ക്രമീകരണങ്ങൾക്ക് മാത്രമാണ്. നിങ്ങളുടെ XMLRPC ക്ലയന്റിനെ (ഉദാ: Windows Live Writer) <strong> ". SITEURL" metaweblog.php </ strong> എന്നതിലേക്ക് പോയി അഭ്യർത്ഥിച്ച ശേഷം മൂല്യങ്ങൾ പൂരിപ്പിക്കുക. മുന്നറിയിപ്പ്! ഈ പ്ലഗിൻ പരീക്ഷണാത്മകമാണ്!");
+define("XMLRPC_HELP_020", "പ്ലഗിൻ ശീർഷകം");
+define("XMLRPC_HELP_021", "പ്ലുഗിൻ ഔട്ട്പുട്ട് ചെയ്യുന്നില്ല എന്ന കാര്യത്തിന് ഉപരിയായി");
+define("XMLRPC_HELP_030", "ന്യൂസ് റെൻഡർ തരം");
+define("XMLRPC_HELP_031", "പ്രധാനപ്പെട്ടത്! ഡീഫോൾട് ന്യൂസ് റെൻഡർ തരം");
+define("XMLRPC_HELP_040", "ഫയലുകൾക്കായുള്ള അപ്ലോഡ് ഫോൾഡർ");
+define("XMLRPC_HELP_041", "ഇത് എല്ലായ്പ്പോഴും e107_files-ൽ ഉൾപ്പെടുന്ന ഒരു ഫോൾഡർ ആണ്!");
+define("XMLRPC_HELP_050", "ക്ലയന്റിനായുള്ള ബ്ലോഗ് ഐഡി");
+define("XMLRPC_HELP_051", "XMLRPC ക്ലയന്റിനായുള്ള ബ്ലോഗ് ഐഡി (നിങ്ങൾക്ക് ആവശ്യമുള്ള ഏതെങ്കിലും സ്ട്രിംഗ്)");
+define("XMLRPC_HELP_060", "ക്ലയന്റിനായുള്ള ബ്ലോഗ് നാമം");
+define("XMLRPC_HELP_061", "XMLRPC ക്ലയന്റിനായുള്ള ബ്ലോഗ് നാമം (നിങ്ങൾക്ക് ആവശ്യമുള്ള ഏതെങ്കിലും സ്ട്രിംഗ്)");
