157502939609dd7205549fccae06b6795385cf22
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-17 14:12:13 +0400

    Add rss_menu plugin terms -complete :100:

---
+++ e107_plugins/rss_menu/languages/Malayalam_admin_rss_menu.php
@@ -0,0 +1,67 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/17 08:23:52
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("RSS_LAN05", "ഇനങ്ങളുടെ എണ്ണം (0 = നിഷ്ക്രിയം)");
+define("RSS_MENU_L1", "ഈ RSS ഫീഡുകൾ ഉപയോഗിച്ച് സിൻഡിക്കേറ്റ് ചെയ്യാം.");
+define("RSS_MENU_L2", "RSS ഫീഡുകൾ");
+define("RSS_MENU_L3", "ഞങ്ങളുടെ ന്യൂസ്");
+define("RSS_MENU_L4", "ഞങ്ങളുടെ അഭിപ്രായങ്ങൾ");
+define("RSS_MENU_L5", "ഞങ്ങളുടെ ഫോറം ത്രെഡുകൾ");
+define("RSS_MENU_L6", "ഞങ്ങളുടെ ഫോറം പോസ്റ്റുകൾ");
+define("RSS_MENU_L7", "ഞങ്ങളുടെ ചാറ്റ്ബോക്സ് പോസ്റ്റുകൾ");
+define("RSS_MENU_L8", "ഞങ്ങളുടെ bugtracker റിപ്പോർട്ടുകൾ");
+define("RSS_MENU_L9", "ഞങ്ങളുടെ ഡൌൺലോഡുകൾ");
+define("RSS_NEWS", "ന്യൂസ്");
+define("RSS_COM", "അഭിപ്രായങ്ങൾ");
+define("RSS_ART", "ലേഖനങ്ങൾ");
+define("RSS_REV", "അവലോകനങ്ങൾ");
+define("RSS_FT", "ഫോറം ത്രെഡുകൾ");
+define("RSS_FP", "ഫോറം പോസ്റ്റുകൾ");
+define("RSS_FSP", "പ്രത്യേക ഫോറം പോസ്റ്റ്");
+define("RSS_BUG", "ബഗ് ട്രാക്കർ");
+define("RSS_FOR", "ഫോറം");
+define("RSS_DL", "ഡൗൺലോഡുകൾ");
+define("RSS_PLUGIN_LAN_6", "ഫീഡ് ലിങ്കുകൾ");
+define("RSS_PLUGIN_LAN_7", "ന്യൂസിന്റെ RSS ഫീഡ്");
+define("RSS_PLUGIN_LAN_8", "ഡൗൺലോഡിന്റെ RSS ഫീഡ്");
+define("RSS_PLUGIN_LAN_9", "അഭിപ്രായങ്ങളുടെ RSS ഫീഡ്");
+define("RSS_PLUGIN_LAN_10", "ന്യൂസ് വിഭാഗത്തിന്റെ RSS ഫീഡ്:");
+define("RSS_PLUGIN_LAN_11", "ഡൗൺലോഡ് വിഭാഗത്തിന്റെ RSS ഫീഡ്:");
+define("RSS_LAN_ADMINMENU_1", "RSS ഓപ്ഷനുകൾ");
+define("RSS_LAN_ADMINMENU_2", "ലിസ്റ്റിംഗ്");
+define("RSS_LAN_ADMINMENU_4", "ഇമ്പോർട്ടു ചെയ്യുക");
+define("RSS_LAN_ERROR_1", "ഇത് ഒരു സാധുവായ rss ഫീഡ് അല്ല.[br][br][RSS ഫീഡ് പട്ടികയിലേക്ക് തിരിച്ചു പോകുക]");
+define("RSS_LAN_ERROR_2", "നിങ്ങളുടെ e107_config.php ഫയലോ നിങ്ങളുടെ ഭാഷാ ഫയലുകളോ സ്പെയ്സുകളോ അല്ലെങ്കിൽ ï»¿﻿ പ്രതീകങ്ങളോ &lt;? പ്രതീകങ്ങൾക്കു മുൻപായി നിലവിലുണ്ട്. ഒരു സാധുവായ RSS ഫീഡ് ഉണ്ടാക്കാൻ നിങ്ങൾ ഇത് ഒരു utf8 ടെക്സ്റ്റ് എഡിറ്ററുപ്രയോഗിച്ച് നീക്കം ചെയ്യണം.");
+define("RSS_LAN_ERROR_3", "RSS ഫീഡുകളൊന്നും ഇതുവരെ ലഭ്യമല്ല[br] ലഭ്യമായ RSS ഫീഡുകൾ ഇംപോർട്ടുചെയ്യാനോ അല്ലെങ്കിൽ RSS ഫീഡുകൾ കരകൃതമായി നിർമ്മിക്കാനോ ദയവായി ഇമ്പോർട്ട് സൗകര്യം ഉപയോഗിക്കുക.");
+define("RSS_LAN_ERROR_4", "RSS ഫീഡുകൾ ഇതുവരെ ലഭ്യമല്ല");
+define("RSS_LAN_ERROR_5", "ഈ RSS എൻട്രി നിലവിലില്ല");
+define("RSS_LAN_ERROR_6", "ഇംപോർട്ടുചെയ്യാൻ rss ഫീഡുകളൊന്നും ഇല്ല");
+define("RSS_LAN_ERROR_7", "ചില ആവശ്യമുള്ള ഫീൽഡുകൾ കാണുന്നില്ല.");
+define("RSS_LAN_ADMIN_1", "നിലവിലുള്ള RSS ഫീഡുകൾ");
+define("RSS_LAN_ADMIN_10", "RSS ഫീഡ് എൻട്രി സൃഷ്ടിക്കുക");
+define("RSS_LAN_ADMIN_11", "RSS ഫീഡ് ഇംപോർട്ട് ഫീഡുകൾ");
+define("RSS_LAN_ADMIN_12", "വിഷയ ID");
+define("RSS_LAN_ADMIN_13", "വാർത്താ ഫീഡിൽ മറ്റ് ന്യൂസ് ഇനങ്ങൾ ഉൾപ്പെടുത്തണോ?");
+define("RSS_LAN_ADMIN_15", "ഇമ്പോർട്ടുചെയ്യാനായി അടയാളപ്പെടുത്താൻ ലിങ്കുകൾ ടിക്ക് ചെയ്യുക ...");
+define("RSS_LAN_ADMIN_16", "ഇമ്പോർട്ട് ചെയ്യട്ടെ?");
+define("RSS_LAN_ADMIN_17", "ടിക്ക് ചെയ്ത ലിങ്കുകൾ ഇമ്പോർട്ടുചെയ്യുക");
+define("RSS_LAN_ADMIN_18", "rss ഫീഡ് (കൾ) ഇമ്പോർട്ട് ചെയ്തു.");
+define("RSS_LAN_ADMIN_19", "വാർത്താ ഫീഡിൽ വിശദീകരണമായി വാർത്താ സംഗ്രഹം ഉപയോഗിക്കണോ?");
+define("RSS_LAN_ADMIN_21", "സജീവവും RSS ഫീഡ് ലിസ്റ്റിൽ ദൃശ്യവുമാണ്");
+define("RSS_LAN_ADMIN_22", "സജീവവും RSS ഫീഡ് ലിസ്റ്റിൽ അദൃശ്യവുവാണ്");
+define("RSS_LAN_ADMIN_23", "നിഷ്ക്രിയം");
+define("RSS_LAN_ADMIN_26", "എല്ലാം ചെക്ക് ചെയ്യുക");
+define("RSS_LAN_ADMIN_27", "എല്ലാം അൺചെക്ക് ചെയ്യുക");
+define("RSS_LAN_ADMIN_31", "RSS എൻട്രി പരിധികൾ പുതുക്കിയിരിക്കുന്നു");
+define("RSS_LAN_ADMIN_33", "വാർത്താ ഫീഡിൽ ചിത്രം കാണിക്കണോ?");
+define("RSS_LAN_0", "RSS");
+define("RSS_LAN_2", "@nospam.com");
+define("RSS_LAN_3", "noauthor@nospam.com");
---
+++ e107_plugins/rss_menu/languages/Malayalam_global.php
@@ -0,0 +1,19 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 14:04:46
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_RSS_NAME", "ആർ.എസ്.എസ്");
+define("LAN_PLUGIN_RSS_DESCRIPTION", "നിങ്ങളുടെ സൈറ്റിൽ നിന്നുള്ള RSS ഫീഡുകൾ.");
+define("LAN_PLUGIN_RSS_SUBSCRIBE", "സബ്സ്ക്രൈബ് ചെയ്യുക");
+define("LAN_PLUGIN_RSS_SUBSCRIBE_TO", "ഇതിനായി [x] സബ്സ്ക്രൈബ് ചെയ്യുക");
+
+
+?>
