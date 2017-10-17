d82b0c7ea227028ec99fe9fa0b1be80b290032a8
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-17 20:56:09 +0400

    Add newsletter plugin terms - complete :100:

---
+++ e107_plugins/newsletter/languages/Malayalam_global.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/15 03:15:47
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_NEWSLETTER_NAME", "ന്യൂസ്-ലെറ്റർ ");
+define("LAN_PLUGIN_NEWSLETTER_DESCRIPTION", "ന്യൂസ്-ലെറ്ററുകൾ ക്രമീകരിക്കാനും അയയ്ക്കാനും വേഗത്തിലും എളുപ്പത്തിലുമുള്ള മാർഗ്ഗം നൽകുന്നു.");
+
+
+?>
