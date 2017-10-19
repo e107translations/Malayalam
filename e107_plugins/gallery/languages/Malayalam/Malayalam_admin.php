955e6d13a4cd997af3c3d718660b0b93264a1d38
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-19 14:25:00 +0400

    Add gallery plugins admin terms - complete

---
+++ e107_plugins/gallery/languages/Malayalam/Malayalam_admin.php
@@ -0,0 +1,80 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/17 08:20:15
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("LAN_GALLERY_ADMIN_01", "[x] സജീവമാണ്. ലളിതമായി ഇമേജുകൾ [y] ഉപയോഗിച്ച് ഗാലറി വിഭാഗത്തിലേക്ക് ഇമ്പോർട്ട് ചെയ്ത് അസൈൻ ചെയ്യുക.");
+define("LAN_GALLERY_ADMIN_03", "സ്ലൈഡ്ഷോ മെനു");
+define("LAN_GALLERY_ADMIN_04", "ഇമേജിന്റെ പരമാവധി വീതി");
+define("LAN_GALLERY_ADMIN_05", "ഇവിടെ നൽകിയിരിക്കുന്ന വീതിയെക്കാൾ വലുതാണെങ്കിൽ ചിത്രങ്ങൾ യാന്ത്രികമായി വലിപ്പം മാറ്റും");
+define("LAN_GALLERY_ADMIN_06", "ഇമേജിന്റെ പരമാവധി ഉയരം");
+define("LAN_GALLERY_ADMIN_07", "ഇവിടെ കൊടുത്തിട്ടുള്ള ഉയരത്തെക്കാൾ കൂടുതലാണെങ്കിൽ യാന്ത്രികമായി ചിത്രങ്ങളുടെ വലിപ്പം മാറ്റും");
+define("LAN_GALLERY_ADMIN_08", "\"ഡൌൺലോഡ് \" ലിങ്ക് കാണിക്കുക");
+define("LAN_GALLERY_ADMIN_09", "ഒരു ഡൌൺലോഡ് ഓപ്ഷൻ പോപ്പപ്പ് ക്യാപ്റ്റിന് അടുത്തായി കാണിക്കും");
+define("LAN_GALLERY_ADMIN_10", "സ്ലൈഡ്ഷോ വിഭാഗം");
+define("LAN_GALLERY_ADMIN_11", "സ്ലൈഡിംഗ് മെനുവിൽ ഈ വിഭാഗത്തിൽ നിന്നുള്ള ചിത്രങ്ങൾ ഉപയോഗിക്കും.");
+define("LAN_GALLERY_ADMIN_12", "സ്ലൈഡ് ദൈർഘ്യം");
+define("LAN_GALLERY_ADMIN_13", "ഒരു മുഴുവൻ ജമ്പിന്റെ ദൈർഘ്യം (സെക്കൻഡുകളിൽ).");
+define("LAN_GALLERY_ADMIN_14", "സ്ലൈഡ് ഓട്ടോ-സ്റ്റാർട്ട്");
+define("LAN_GALLERY_ADMIN_15", "പ്രാപ്തമാക്കുമ്പോൾ പേജ് ലോഡ് ചെയ്യുന്ന സമയത്ത് ഇമേജ്-റൊട്ടേഷൻ യാന്ത്രികമായി ആരംഭിക്കുന്നു.");
+define("LAN_GALLERY_ADMIN_16", "സ്ലൈഡ് ആവൃത്തി");
+define("LAN_GALLERY_ADMIN_17", "യാന്ത്രിക-സമാരംഭം പ്രവർത്തനക്ഷമമാക്കുമ്പോൾ, അത് അടുത്തുള്ള ജമ്പിനു  മുമ്പായി എത്രത്തോളം സ്ലൈഡുകൾ സൂക്ഷിക്കുന്നു എന്ന് വ്യക്തമാക്കുന്നു.");
+define("LAN_GALLERY_ADMIN_18", "സ്ലൈഡ് ഇഫക്റ്റ്");
+define("LAN_GALLERY_ADMIN_19", "എഫക്ടിന്റെ തരം.");
+define("LAN_GALLERY_ADMIN_20", "ഓരോ പേജിലുമുള്ള ചിത്രങ്ങൾ");
+define("LAN_GALLERY_ADMIN_21", "ഓരോ പേജിലും കാണിക്കേണ്ട ഇമേജുകളുടെ എണ്ണം");
+define("LAN_GALLERY_ADMIN_22", "ചിത്രങ്ങൾ ക്രമപ്പെടുത്തുക");
+define("LAN_GALLERY_ADMIN_23", "മീഡിയ ഐഡി ആരോഹണക്രമം");
+define("LAN_GALLERY_ADMIN_24", "മീഡിയ ഐഡി അവരോഹണക്രമം");
+define("LAN_GALLERY_ADMIN_25", "മീഡിയ നാമം ആരോഹണക്രമം");
+define("LAN_GALLERY_ADMIN_26", "മീഡിയ നാമം അവരോഹണക്രമം");
+define("LAN_GALLERY_ADMIN_27", "മീഡിയ ക്യാപ്ഷൻ ആരോഹണക്രമം");
+define("LAN_GALLERY_ADMIN_28", "മീഡിയ ക്യാപ്ഷൻ അവരോഹണക്രമം");
+define("LAN_GALLERY_ADMIN_29", "ഇടത്തേക്ക് സ്ലൈഡ് ചെയ്യുക");
+define("LAN_GALLERY_ADMIN_30", "താഴേക്കു സ്ലൈഡ് ചെയ്യുക");
+define("LAN_GALLERY_ADMIN_31", "മങ്ങിക്കുക");
+define("LAN_GALLERY_ADMIN_32", "prettyPhoto ക്രമീകരണങ്ങൾ");
+define("LAN_GALLERY_ADMIN_33", "ആനിമേഷൻ വേഗത");
+define("LAN_GALLERY_ADMIN_34", "സ്ലൈഡ്ഷോ: ഇടവേള സമയം");
+define("LAN_GALLERY_ADMIN_35", "സ്ലൈഡ്ഷോ: ഓട്ടോപ്ലേ");
+define("LAN_GALLERY_ADMIN_36", "അതാര്യത");
+define("LAN_GALLERY_ADMIN_37", "0 മുതൽ 1 വരെയുള്ള മൂല്യം.");
+define("LAN_GALLERY_ADMIN_38", "ശീർഷകം കാണിക്കുക");
+define("LAN_GALLERY_ADMIN_39", "വലുപ്പം മാറ്റാൻ അനുവദിക്കുക");
+define("LAN_GALLERY_ADMIN_40", "വ്യൂപോർട്ടിനേക്കാൾ വലുതായ ഫോട്ടോകൾ വലുപ്പം മാറ്റുക.");
+define("LAN_GALLERY_ADMIN_41", "ഡീഫോൾട് വീതി");
+define("LAN_GALLERY_ADMIN_42", "ഡീഫോൾട് ഉയരം");
+define("LAN_GALLERY_ADMIN_43", "കൌണ്ടർ വിഭാജി ലേബൽ");
+define("LAN_GALLERY_ADMIN_44", "ഗാലറി കൌണ്ടർ 1 \"ലെ \" 2 എന്ന സെപ്പറേറ്റർ");
+define("LAN_GALLERY_ADMIN_46", "തിരശ്ചീന പാഡിംഗ്");
+define("LAN_GALLERY_ADMIN_47", "ചിത്രത്തിന്റെ ഓരോ വശത്തിലുമുള്ള പാഡിംഗ്.");
+define("LAN_GALLERY_ADMIN_48", "ഫ്ലാഷ് മറയ്ക്കുക");
+define("LAN_GALLERY_ADMIN_49", "ഒരു പേജിലെ എല്ലാ ഫ്ലാഷ് ഒബ്ജക്റ്റുകളും മറക്കുന്നു, prettyPhoto ഫ്ലാഷ് ദൃശ്യമാകുന്നെങ്കിൽ ഇത് TRUE ആയി സജ്ജീകരിക്കുക.");
+define("LAN_GALLERY_ADMIN_50", "Wmode");
+define("LAN_GALLERY_ADMIN_51", "ഫ്ലാഷ് wmode ആട്രിബ്യൂട്ട് സജ്ജമാക്കുക.");
+define("LAN_GALLERY_ADMIN_52", "വീഡിയോകൾ ഓട്ടോപ്ലേ ചെയ്യുക");
+define("LAN_GALLERY_ADMIN_53", "മൊഡൽ ക്ലോസ്");
+define("LAN_GALLERY_ADMIN_54", "true ആയി ക്രമീകരിച്ചിരിക്കുന്നുവെങ്കിൽ, ക്ലോസ് ബട്ടണ് മാത്രം ജാലകം അടയ്ക്കും.");
+define("LAN_GALLERY_ADMIN_55", "ഡീപ്-ലിങ്കിംഗ്");
+define("LAN_GALLERY_ADMIN_56", "ഡീപ്-ലിങ്കിംഗ് പ്രാപ്തമാക്കാൻ url അപ്ഡേറ്റുചെയ്യുന്നതിന് prettyPhoto-യെ അനുവദിക്കുക.");
+define("LAN_GALLERY_ADMIN_57", "ഓവർലേ ഗാലറി");
+define("LAN_GALLERY_ADMIN_58", "True ആയി സജ്ജീകരിച്ചിട്ടുണ്ടെങ്കിൽ, ഒരു ഗാലറി മൗസ്സ്-ഓവർ ചെയ്യുമ്പോൾ പൂർണ്ണസ്ക്രീൻ ഇമേജ് ഓവർലേ ചെയ്യും.");
+define("LAN_GALLERY_ADMIN_59", "കീബോർഡ് കുറുക്കുവഴികൾ");
+define("LAN_GALLERY_ADMIN_60", "PrettyPhoto- ൽ നിങ്ങൾ ഫോമുകൾ തുറക്കുന്നെങ്കിൽ false ആയി സജ്ജമാക്കുക.");
+define("LAN_GALLERY_ADMIN_61", "IE6 ഫാൾബാക്ക്");
+define("LAN_GALLERY_ADMIN_62", "വേഗം");
+define("LAN_GALLERY_ADMIN_63", "പതുക്കെ");
+define("LAN_GALLERY_ADMIN_64", "സാധാരണ");
+define("LAN_GALLERY_ADMIN_65", "ലൈറ്റ് റൗണ്ട് ചെയ്ത");
+define("LAN_GALLERY_ADMIN_66", "ലൈറ്റ് റൗണ്ട് ചെയ്ത");
+define("LAN_GALLERY_ADMIN_67", "ലൈറ്റ് ചതുരശ്രമായ");
+define("LAN_GALLERY_ADMIN_68", "ഇരുണ്ട ചതുരശ്രമായ");
+define("LAN_GALLERY_ADMIN_69", "ഫേസ്ബുക്ക്");
+define("LAN_GALLERY_ADMIN_70", "ആഗോളമായി prettyPhoto ലോഡ് ചെയ്യുക");
+define("LAN_GALLERY_ADMIN_71", "prettyPhoto ആട്രിബ്യൂട്ട് (ഹുക്ക്)");
