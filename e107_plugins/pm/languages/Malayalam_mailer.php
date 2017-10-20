7cc1009f88970b1ff1de7da58b0df167489765ac
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-20 22:43:33 +0400

    Add pm plugins admin, main, mailer files - complete

---
+++ e107_plugins/pm/languages/Malayalam_mailer.php
@@ -0,0 +1,21 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 14:09:23
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("LAN_EC_PM_01", "");
+define("LAN_EC_PM_02", "");
+define("LAN_EC_PM_03", "");
+define("LAN_EC_PM_04", "PM ഹാൻഡ്ലർ");
+define("LAN_EC_PM_05", " പി.എം ഇന്റെ വലിയ അയക്കൽ നടപടിക്രമം നടത്തുക.");
+define("LAN_EC_PM_06", "--NUM-- സ്വീകർത്താക്കൾക്ക് വേണ്ടിയുള്ള ബൾക്ക് പി.എം പ്രോസസ്സിംഗ് ആരംഭിക്കുക");
+define("LAN_EC_PM_07", "");
+
+
+?>
