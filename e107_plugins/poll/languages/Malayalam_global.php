775615962afeb9934e99d24b0c57482c9d76b84d
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-20 22:47:12 +0400

    Add poll global, admin files - complete

---
+++ e107_plugins/poll/languages/Malayalam_global.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 13:38:35
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_POLL_NAME", "പോൾ ");
+define("LAN_PLUGIN_POLL_DESCRIPTION", "പോൾ പ്ലഗിൻ മെനു അല്ലെങ്കിൽ ഫോറം പോസ്റ്റിൽ വോട്ടെടുപ്പ് നിർവ്വചിക്കാൻ നിങ്ങളെ അനുവദിക്കുന്നു.");
+
+
+?>
