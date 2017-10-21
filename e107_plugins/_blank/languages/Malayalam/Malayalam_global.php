e004748f647a1cbbbf433f1a5098b8cf7f947d07
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-21 08:41:10 +0400

    Add _blank plugin complete

---
+++ e107_plugins/_blank/languages/Malayalam/Malayalam_global.php
@@ -0,0 +1,18 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/26 22:18:23
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN__BLANK_NAME", "ശൂന്യ പ്ലഗിൻ");
+define("LAN_PLUGIN__BLANK_DIZ", "പ്ളഗിൻ വികസനത്തിൽ നിങ്ങൾക്ക് ആരംഭിക്കുന്നതിന് സഹായിക്കുന്ന ഒരു ശൂന്യ പ്ലഗിൻ. കൂടുതൽ വിവരങ്ങൾ ഇവിടെ ചേർക്കാവുന്നതാണ്.");
+define("LAN_PLUGIN__BLANK_LINK", "ശൂന്യ ലിങ്ക്");
+
+
+?>
