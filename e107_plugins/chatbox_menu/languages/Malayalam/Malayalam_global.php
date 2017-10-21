76173045ace5a50b5ddd12aae9558cee959cce46
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-21 08:45:14 +0400

    Add chatbox plugin - complete

---
+++ e107_plugins/chatbox_menu/languages/Malayalam/Malayalam_global.php
@@ -0,0 +1,28 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/27 23:50:33
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_CHATBOX_MENU_NAME", "ചാറ്റ്ബോക്സ്");
+define("LAN_PLUGIN_CHATBOX_MENU_DESCRIPTION", "ചാറ്റ്ബോക്സ് മെനു");
+define("LAN_PLUGIN_CHATBOX_MENU_POSTS", "ചാറ്റ്ബോക്സ് പോസ്റ്റുകൾ");
+define("LAN_AL_CHBLAN_01", "ചാറ്റ്ബോക്സ് ക്രമീകരണങ്ങൾ അപ്ഡേറ്റുചെയ്തു");
+define("LAN_AL_CHBLAN_02", "ചാറ്റ്ബോക്സ് വൃത്തിയാക്കി");
+define("LAN_AL_CHBLAN_03", "ചാറ്റ്ബോക്സ് പോസ്റ്റുകൾ പുനഃർനിര്‍ണ്ണയിക്കപ്പെട്ടു");
+define("LAN_AL_CHBLAN_04", "");
+define("LAN_AL_CHBLAN_05", "");
+define("NT_LAN_CB_1", "ചാറ്റ്ബോക്സ് ഇവന്റുകൾ");
+define("NT_LAN_CB_2", "സന്ദേശം പോസ്റ്റുചെയ്തു");
+define("NT_LAN_CB_3", "പോസ്റ്റ് ചെയ്തത്");
+define("NT_LAN_CB_5", "സന്ദേശം");
+define("NT_LAN_CB_6", "ചാറ്റ്ബോക്സ് സന്ദേശം പോസ്റ്റുചെയ്തു");
+
+
+?>
