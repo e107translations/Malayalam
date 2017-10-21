cd357d10a06938fd8cc69fa24bc5e31f3e797f0e
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-21 08:47:38 +0400

    Add blogcalendar_menu - complete

---
+++ e107_plugins/blogcalendar_menu/languages/Malayalam.php
@@ -0,0 +1,22 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/27 23:45:36
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("BLOGCAL_L1", "മാസാനുസരണമുള്ള വാർത്ത");
+define("BLOGCAL_L2", "ആർക്കൈവ്");
+define("BLOGCAL_1", "വാർത്താ ഇനങ്ങൾ");
+define("BLOGCAL_CONF1", "മാസങ്ങൾ/വരി");
+define("BLOGCAL_CONF2", "സെൽപാഡിംഗ്");
+define("BLOGCAL_CONF4", "BlogCal മെനു കോൺഫിഗറേഷൻ");
+define("BLOGCAL_ARCHIV1", "ആർക്കൈവ് തിരഞ്ഞെടുക്കുക");
+
+
+?>
