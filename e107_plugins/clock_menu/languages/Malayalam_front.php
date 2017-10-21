61666f23f6e80b984410566f992af8a6b08bddf7
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-21 08:48:59 +0400

    Add clock_menu - complete

---
+++ e107_plugins/clock_menu/languages/Malayalam_front.php
@@ -0,0 +1,38 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 15:12:01
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("CLOCK_MENU_L1", "ക്ലോക്ക് മെനു കോൺഫിഗറേഷൻ സംരക്ഷിച്ചു");
+define("CLOCK_MENU_L2", "ക്യാപ്ഷൻ");
+define("CLOCK_MENU_L3", "അപ്ഡേറ്റ് ക്രമീകരണങ്ങൾ മെനു");
+define("CLOCK_MENU_L4", "ക്ലോക്ക് മെനു കോൺഫിഗ്");
+define("CLOCK_MENU_L5", "തിങ്കളാഴ്ച");
+define("CLOCK_MENU_L6", "ചൊവ്വാഴ്ച");
+define("CLOCK_MENU_L7", "ബുധനാഴ്ച");
+define("CLOCK_MENU_L8", "വ്യാഴാഴ്ച");
+define("CLOCK_MENU_L9", "വെള്ളിയാഴ്ച");
+define("CLOCK_MENU_L10", "ശനിയാഴ്ച");
+define("CLOCK_MENU_L11", "ഞായറാഴ്ച");
+define("CLOCK_MENU_L12", "ജനുവരി");
+define("CLOCK_MENU_L13", "ഫെബ്രുവരി");
+define("CLOCK_MENU_L14", "മാർച്ച്");
+define("CLOCK_MENU_L15", "ഏപ്രിൽ");
+define("CLOCK_MENU_L16", "മെയ്");
+define("CLOCK_MENU_L17", "ജൂൺ");
+define("CLOCK_MENU_L18", "ജൂലൈ");
+define("CLOCK_MENU_L19", "ഓഗസ്റ്റ്");
+define("CLOCK_MENU_L20", "സെപ്റ്റംബർ");
+define("CLOCK_MENU_L21", "ഒക്ടോബർ");
+define("CLOCK_MENU_L22", "നവംബർ");
+define("CLOCK_MENU_L23", "ഡിസംബർ");
+
+
+?>
