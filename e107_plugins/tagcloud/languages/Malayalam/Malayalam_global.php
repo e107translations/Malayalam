b230f36f3a9c0c0ce716992181355d6c5e20391c
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-21 09:48:31 +0400

    Add tagcloud plugin - complete

---
+++ e107_plugins/tagcloud/languages/Malayalam/Malayalam_global.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 13:57:04
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_TAGCLOUD_NAME", "ടാഗ് ക്ലൗഡ്");
+define("LAN_PLUGIN_TAGCLOUD_DESCRIPTION", "നിങ്ങളുടെ e107 വെബ്സൈറ്റിന് ഒരു ലളിതമായ ടാഗ് ക്ലൗഡ് മെനു.");
+
+
+?>
