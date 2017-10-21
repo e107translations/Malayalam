8d888fbd2d365683bec00ae727b98df882f65c52
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-21 08:53:08 +0400

    Add featurebox admin, global files

---
+++ e107_plugins/featurebox/languages/Malayalam_global.php
@@ -0,0 +1,23 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/06/05 19:27:58
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_FEATUREBOX_NAME", "ഫീച്ചർ ബോക്സ്");
+define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "നിങ്ങളുടെ പേജിന്റെ മുകളിലുള്ള ഒരു ആനിമേറ്റഡ് പ്രദേശം വാർത്താ ഇനങ്ങളും നിങ്ങൾ ആഗ്രഹിക്കുന്ന മറ്റ് ഫീച്ചറുകളും ഉൾപ്പെടുത്താൻ ഉദ്ദേശിച്ചുള്ളത്.");
+define("LAN_PLUGIN_FEATUREBOX_BATCH", "ഫീച്ചർ ബോക്സ് ഇനം സൃഷ്ടിക്കുക");
+define("FBLAN_INSTALL_01", "ഡീഫോൾട് വിഭാഗ ഡാറ്റ പട്ടിക ചേർക്കുന്നു.");
+define("FBLAN_INSTALL_02", "ഡീഫോൾട് പട്ടികയുടെ വിവരം ചേർക്കുന്നു.");
+define("FBLAN_INSTALL_03", "നൽകിയിട്ടില്ല");
+define("FBLAN_INSTALL_04", "കറൗസൽ");
+define("FBLAN_INSTALL_05", "ടാബുകൾ");
+
+
+?>
