cb7b95ba4fa69eec2a6aae635d8bc874d3fc9987
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-21 08:42:39 +0400

    Add banner plugin admin, global, front files

---
+++ e107_plugins/banner/languages/Malayalam_front.php
@@ -0,0 +1,29 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/26 22:24:05
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("BANNERLAN_19", "തുടരുന്നതിനായി നിങ്ങളുടെ ക്ലയന്റ് ലോഗിൻ, പാസ്സ്വേർഡ് എന്നിവ നൽകുക");
+define("BANNERLAN_20", "ക്ഷമിക്കണം, ആ വിശദാംശങ്ങൾ ഡാറ്റാബേസിൽ കണ്ടെത്താനായില്ല. വിശദാംശങ്ങൾക്ക് സൈറ്റ് അഡ്മിനിസ്ട്രേറ്ററുമായി ബന്ധപ്പെടുക.");
+define("BANNERLAN_21", "ബാനർ സ്റ്റാറ്റിസ്റ്റിക്സ്");
+define("BANNERLAN_22", "കക്ഷി");
+define("BANNERLAN_23", "ബാനർ ഐ.ഡി");
+define("BANNERLAN_24", "ക്ലിക്ക് ത്രൂകൾ");
+define("BANNERLAN_25", "ക്ലിക്ക് %");
+define("BANNERLAN_26", "ഇംപ്രഷനുകൾ");
+define("BANNERLAN_27", "വാങ്ങിയ ഇംപ്രഷനുകൾ");
+define("BANNERLAN_28", "ശേഷിക്കുന്ന ഇംപ്രഷനുകൾ");
+define("BANNERLAN_30", "പരിധിയില്ലാത്ത");
+define("BANNERLAN_31", "ബാധകമല്ല");
+define("BANNERLAN_35", "ക്ലിക്ക് ത്രൂ ഐ.പി വിലാസങ്ങൾ");
+define("BANNERLAN_39", "ഈ ബാനറിനായി ഇമേജ് ഒന്നും നിശ്ചയിച്ചിട്ടില്ല.");
+
+
+?>
---
+++ e107_plugins/banner/languages/Malayalam_global.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/27 23:43:58
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_BANNER_NAME", "ബാനറുകൾ");
+define("LAN_PLUGIN_BANNER_DESCRIPTION", "നിങ്ങളുടെ e107 വെബ്സൈറ്റിലേക്ക് പരസ്യബാനറുകൾ ചേർക്കുക");
+
+
+?>
