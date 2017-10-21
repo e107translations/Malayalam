2f7ec8d6bd5a000734e984ad2537d5ad175ab5bc
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-21 09:50:46 +0400

    Add trackback plugin - complete

---
+++ e107_plugins/trackback/languages/Malayalam_admin_trackback.php
@@ -0,0 +1,22 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 13:53:39
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("TRACKBACK_L7", "ട്രാക്ക്ബാക് സജീവമാക്കുക");
+define("TRACKBACK_L8", "ട്രാക്ക്ബാക് URL- ടെക്സ്റ്റ്");
+define("TRACKBACK_L10", "ട്രാക്ക്ബാക് ക്രമീകരണങ്ങൾ");
+define("TRACKBACK_L11", "ഈ പോസ്റ്റിനായുള്ള ട്രാക്ക്ബാക് വിലാസം:");
+define("TRACKBACK_L12", "ഈ ഇനത്തിനായുള്ള ട്രാക്ക്ബാക്ക്സ് ഒന്നുമില്ല");
+define("TRACKBACK_L13", "ട്രാക്ക്ബാക്ക് മോഡറേറ്റ് ചെയ്യുക");
+define("TRACKBACK_L16", "ട്രാക്ക്ബാക്ക്");
+
+
+?>
---
+++ e107_plugins/trackback/languages/Malayalam_global.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 13:55:39
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_TRACKBACK_NAME", "ട്രാക്ക്ബാക്ക്");
+define("LAN_PLUGIN_TRACKBACK_DESCRIPTION", "നിങ്ങളുടെ ന്യൂസ് പോസ്റ്റുകൾക്ക് ട്രാക്ക്ബാക്ക് ഉപയോഗിക്കാൻ ഈ പ്ലഗിൻ നിങ്ങളെ സഹായിക്കുന്നു.");
+
+
+?>
