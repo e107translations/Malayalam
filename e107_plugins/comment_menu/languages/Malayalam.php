73c4aaf0ca1ff56e5e6a4f8716eb7a993e4ff3ea
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-21 08:49:46 +0400

    Add comment_menu - complete

---
+++ e107_plugins/comment_menu/languages/Malayalam.php
@@ -0,0 +1,27 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 13:37:07
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("CM_L1", "ഇതുവരെ അഭിപ്രായങ്ങളൊന്നും ഇല്ല.");
+define("CM_L2", "");
+define("CM_L3", "ക്യാപ്ഷൻ");
+define("CM_L4", "പ്രദർശിപ്പിക്കുന്നതിനായി കമന്റുകൾ എണ്ണം?");
+define("CM_L5", "പ്രദർശിപ്പിക്കാനുള്ള അക്ഷരങ്ങൾ എണ്ണം?
+");
+define("CM_L6", "ദൈർഘ്യമേറിയതാണ് അഭിപ്രായങ്ങൾക്കായി postfix?");
+define("CM_L7", "മെനുവിൽ യഥാർത്ഥ വാർത്ത ശീർഷകം കാണിക്കുക?");
+define("CM_L8", "പുതിയ അഭിപ്രായങ്ങൾ മെനു - സജ്ജീകരണങ്ങൾ");
+define("CM_L11", "മേൽ");
+define("CM_L12", "വീണ്ടും:");
+define("CM_L13", "പോസ്റ്റ് ചെയ്തത്");
+
+
+?>
