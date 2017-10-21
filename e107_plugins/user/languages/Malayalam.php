62b61b4b467850dafbbf7bd627184e6462344dec
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-21 09:51:21 +0400

    Add user plugin - complete

---
+++ e107_plugins/user/languages/Malayalam.php
@@ -0,0 +1,28 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/27 23:56:38
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("UTHEME_MENU_L1", "സജ്ജമാക്കുക");
+define("UTHEME_MENU_L2", "ഭാഷ തിരഞ്ഞെടുക്കുക");
+define("UTHEME_MENU_L3", "പട്ടികകൾ");
+define("LAN_UMENU_THEME_1", "തീം സജ്ജമാക്കുക");
+define("LAN_UMENU_THEME_2", "തീം തിരഞ്ഞെടുക്കുക");
+define("LAN_UMENU_THEME_3", "ഉപയോക്താക്കൾ:");
+define("LAN_UMENU_THEME_4", "ഉപയോക്താക്കൾ തിരഞ്ഞെടുക്കാൻ കഴിയുന്ന ആ തീമുകൾ പ്രവർത്തനക്ഷമമാക്കുക");
+define("LAN_UMENU_THEME_5", "അപ്ഡേറ്റ്
+");
+define("LAN_UMENU_THEME_6", "ഉപയോക്താക്കൾക്ക് ലഭ്യമായ തീമുകൾ");
+define("LAN_UMENU_THEME_7", "തീമുകൾ തിരഞ്ഞെടുക്കാൻ കഴിയുന്ന ക്ലാസ്");
+define("LAN_UMENU_THEME_8", "അനുവദിച്ച തീമുകൾ:");
+define("LAN_UMENU_THEME_9", "തീമുകൾ തിരഞ്ഞെടുക്കാൻ കഴിയുന്ന ക്ലാസ്:");
+
+
+?>
---
+++ e107_plugins/user/languages/Malayalam_global.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/27 23:57:26
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_USER_NAME", "ഉപയോക്താവ്");
+define("LAN_PLUGIN_USER_DESC", "ഉപയോക്തൃ തീമും ഭാഷ മെനുകളും");
+
+
+?>
