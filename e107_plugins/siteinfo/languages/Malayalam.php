14d53b0717920aca592afb49e9a305c10daa1b10
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-21 08:57:43 +0400

    Add siteinfo plugin - complete

---
+++ e107_plugins/siteinfo/languages/Malayalam.php
@@ -0,0 +1,27 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/29 13:50:19
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("COMPLIANCE_L1", "W3C അനുവര്‍ത്തനം");
+define("SITEBUTTON_MENU_L1", "ഞങ്ങളുമായി ബന്ധിപ്പിക്കുക");
+define("POWEREDBY_L1", "പ്രാപ്തമാക്കിയത്");
+define("COUNTER_L1", "അഡ്മിൻ സന്ദർശനങ്ങൾ കണക്കാക്കപ്പെടുന്നില്ല.");
+define("COUNTER_L2", "ഇന്ന് ഈ പേജ് ...");
+define("COUNTER_L3", "മൊത്തം");
+define("COUNTER_L4", "ഈ പേജ് എന്നെന്നേക്കും ...");
+define("COUNTER_L5", "അതുല്യമായ");
+define("COUNTER_L6", "സൈറ്റ് ...");
+define("COUNTER_L7", "കൌണ്ടർ");
+define("COUNTER_L8", "അഡ്മിൻ സന്ദേശം: <b>സ്റ്റാഗ് ലോഗിംഗ് അപ്രാപ്തമാക്കി. </b> <br/> സജീവമാക്കുന്നതിന്, നിങ്ങളുടെ <a href='".e_ADMIN."plugin.php'> പ്ലഗിൻ മാനേജറിൽ നിന്ന്</a> സ്റാറ്റിസ്റിക് ലോഗിംഗ് പ്ലഗിൻ ഇൻസ്റ്റാൾ ചെയ്യണം 
+തുടർന്ന് <a href='".e_PLUGIN."log/admin_config.php'> കോൺഫിഗറേഷൻ സ്ക്രീൻ  </a> സന്ദർശിച്ച് അത് സജീവമാക്കുക.");
+
+
+?>
