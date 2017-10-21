e2f37bdd8c23730168c6bdcc2e29d0acf343f595
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-10-21 09:49:30 +0400

    Add tinymce4 plugin - complete

---
+++ e107_plugins/tinymce4/languages/Malayalam_admin.php
@@ -0,0 +1,21 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/27 23:59:39
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("TMCEALAN_1", "ഡീഫോൾട് വാചകമായി ഒട്ടിക്കുക");
+define("TMCEALAN_2", "ബ്രൗസർ അക്ഷരപ്പിശക് പരിശോധന");
+define("TMCEALAN_3", "ബ്രൗസർ ആന്തരിക സ്പെൽചെക്കർ ഉപയോഗിച്ചിട്ടുണ്ടെങ്കിൽ ഇത് പ്രവർത്തനക്ഷമമാക്കുക.");
+define("TMCEALAN_4", "വിഷ്വൽ ബ്ലോക്കുകൾ
+");
+define("TMCEALAN_5", "എഡിറ്റുചെയ്യുന്ന സമയത്ത് html ബ്ലോക്കുകൾ കാണിക്കാൻ ഇത് സജ്ജമാക്കുക.");
+
+
+?>
