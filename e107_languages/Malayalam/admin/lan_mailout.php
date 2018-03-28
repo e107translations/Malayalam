8c6037e04dde6b9638735b4312b7cc3f84dea5fd
Arun S. Sekher <arunshekher@users.noreply.github.com>
2018-03-28 18:02:06 +0400

    Add mailout e107_languages/Malayalam/admin/lan_mailout.php

---
+++ e107_languages/Malayalam/admin/lan_mailout.php
@@ -0,0 +1,38 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2018/03/28 13:59:14
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("CORE_DATE_ORDER", "ദിമവ");
+define("LAN_MAILOUT_01", "ഉത്ഭവ പേര് ");
+define("LAN_MAILOUT_02", "ഉത്ഭവ ഇമെയിൽ");
+define("LAN_MAILOUT_03", "വരെ");
+define("LAN_MAILOUT_04", "സി.സി");
+define("LAN_MAILOUT_05", "ബി.സി.സി");
+define("LAN_MAILOUT_06", "വിഷയം");
+define("LAN_MAILOUT_07", "തൊങ്ങല്‍");
+define("LAN_MAILOUT_08", "ഇമെയിൽ അയയ്ക്കുക");
+define("LAN_MAILOUT_09", "അയക്കേണ്ട ഫോർമാറ്റ് ");
+define("LAN_MAILOUT_10", "ഉപയോക്താവ് സബ്സ്ക്രൈബ്ചെയ്തു");
+define("LAN_MAILOUT_11", "വേരിയബിളുകൾ തിരുകുക");
+define("LAN_MAILOUT_12", "എല്ലാ ഉപയോക്താക്കളും");
+define("LAN_MAILOUT_13", "എല്ലാ സ്ഥിരീകരിക്കാത്ത ഉപയോക്താക്കളും");
+define("LAN_MAILOUT_14", "പ്രദർശന നാമം");
+define("LAN_MAILOUT_15", "മെയിൽഔട്ട്");
+define("LAN_MAILOUT_16", "ഉപയോക്തൃനാമം");
+define("LAN_MAILOUT_17", "സൈൻ അപ്പ് ലിങ്ക്");
+define("LAN_MAILOUT_18", "യൂസർ ഐഡി");
+define("LAN_MAILOUT_19", "ടാർഗെറ്റ് ഇമെയിൽ വിലാസമൊന്നും വ്യക്തമാക്കിയിട്ടില്ല");
+define("LAN_MAILOUT_20", "Sendmail-പാത്ത്");
+define("LAN_MAILOUT_21", "ബൾക്ക് മെയിൽ എൻട്രികൾ");
+define("LAN_MAILOUT_22", "നിലവിൽ സംരക്ഷിച്ച എൻട്രികൾ ഒന്നുമില്ല");
+define("LAN_MAILOUT_23", "ഉപയോക്തൃക്ലാസ്:");
+define("LAN_MAILOUT_24", "ഇമെയിൽ(കൾ) അയയ്ക്കാൻ തയ്യാറാണ്");
+define("LAN_MAILOUT_25", "ബൾക്ക് മെയിലിംഗ് നിയന്ത്രണങ്ങൾ");
